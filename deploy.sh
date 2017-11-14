28e3ffe978accc09be8d9c146a881de4dcfecf54
SquidDev <bonzoweb@hotmail.co.uk>
2017-11-14 22:37:37 +0000

    Update Gradle and build system
     - Bundle Javadoc and APIs using gradle instead of deploy.sh
     - Bump Gradle to 4.3, significantly improving compile times.

--- deploy.sh
+++ deploy.sh
@@ -18,21 +18,4 @@ OUTPUTJAR=`ls -1 build/libs | grep -v sources | sed s/\-//g`
 FRIENDLYNAME=`ls -1 build/libs | grep -v sources | sed s/\-/\ /g | sed s/\.jar//g`
 cp build/libs/$INPUTJAR deploy/$OUTPUTJAR
 
-echo "Creating API..."
-mkdir -p deploy/api/src/dan200/computercraft
-cp -r build/sources/main/java/dan200/computercraft/api deploy/api/src/dan200/computercraft/api
-
-echo "Creating API Javadocs..."
-mkdir -p deploy/api/doc
-cd src/main/java/dan200/computercraft/api
-find . -type f -name "*.java" | xargs javadoc -d ../../../../../../deploy/api/doc -windowtitle "$FRIENDLYNAME"
-cd ../../../../../..
-
-echo "Adding API and Javadocs to deployment..."
-cd deploy
-zip -r $OUTPUTJAR api/doc > /dev/null
-zip -r $OUTPUTJAR api/src/dan200/computercraft > /dev/null
-cd ..
-rm -rf deploy/api
-
 echo "Done."
