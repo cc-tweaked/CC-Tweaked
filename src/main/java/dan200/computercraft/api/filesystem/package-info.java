7bc9745c7a368d2ba75e70c657ac052ffabe0aca
SquidDev <bonzoweb@hotmail.co.uk>
2019-01-03 11:02:11 +0000

    Remove redundant line in copyright header
    Really should have noticed this when committing it.

--- src/main/java/dan200/computercraft/api/filesystem/package-info.java
+++ src/main/java/dan200/computercraft/api/filesystem/package-info.java
@@ -2,7 +2,6 @@
  * This file is part of the public ComputerCraft API - http://www.computercraft.info
  * Copyright Daniel Ratcliffe, 2011-2019. This API may be redistributed unmodified and in full only.
  * For help using the API, and posting your mods, visit the forums at computercraft.info.
- *
  */
 
 @API( owner = "ComputerCraft", provides = "ComputerCraft|API|FileSystem", apiVersion = "${version}" )
