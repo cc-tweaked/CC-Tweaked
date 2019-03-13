dan200/ComputerCraft

os.time() and os.day() join the "Finally Case-Insensitive Party"

src/main/java/dan200/computercraft/core/apis/OSAPI.java
@@ -303,7 +303,7 @@ public class OSAPI implements ILuaAPI
             {
                 // time
                 String param = optString( args, 0, "ingame" );
-                switch( param )
+                switch( param.toLowerCase( Locale.ENGLISH ) )
                 {
                     case "utc":
                     {
@@ -331,7 +331,7 @@ public class OSAPI implements ILuaAPI
             {
                 // day
                 String param = optString( args, 0, "ingame" );
-                switch( param )
+                switch( param.toLowerCase( Locale.ENGLISH ) )
                 {
                     case "utc":
                     {
@@ -385,7 +385,7 @@ public class OSAPI implements ILuaAPI
             {
                 // epoch
                 String param = optString( args, 0, "ingame" );
-                switch( param.toLowerCase() )
+                switch( param.toLowerCase( Locale.ENGLISH ) )
                 {
                     case "utc":
                     {

cc-tweaked/CC-Tweaked

src/main/java/dan200/computercraft/core/apis/OSAPI.java
@@ -298,7 +298,7 @@ public class OSAPI implements ILuaAPI
             {
                 // time
                 String param = optString( args, 0, "ingame" );
-                switch( param )
+                switch( param.toLowerCase( Locale.ROOT ) )
                 {
                     case "utc":
                     {
@@ -326,7 +326,7 @@ public class OSAPI implements ILuaAPI
             {
                 // day
                 String param = optString( args, 0, "ingame" );
-                switch( param )
+                switch( param.toLowerCase( Locale.ROOT ) )
                 {
                     case "utc":
                     {
@@ -380,7 +380,7 @@ public class OSAPI implements ILuaAPI
             {
                 // epoch
                 String param = optString( args, 0, "ingame" );
-                switch( param )
+                switch( param.toLowerCase( Locale.ROOT ) )
                 {
                     case "utc":
                     {