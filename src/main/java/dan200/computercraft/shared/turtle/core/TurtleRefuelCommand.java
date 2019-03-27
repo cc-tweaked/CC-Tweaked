6ed03e1fcddeae0829a977ae75e440cc0d44d9b2
SquidDev <bonzoweb@hotmail.co.uk>
2019-03-27 20:38:39 +0000

    Convert turtle refuelling into an event
    This should allow us (or other mods) to register custom refuel handlers
    should they so wish.

--- src/main/java/dan200/computercraft/shared/turtle/core/TurtleRefuelCommand.java
+++ src/main/java/dan200/computercraft/shared/turtle/core/TurtleRefuelCommand.java
@@ -10,92 +10,36 @@ import dan200.computercraft.api.turtle.ITurtleAccess;
 import dan200.computercraft.api.turtle.ITurtleCommand;
 import dan200.computercraft.api.turtle.TurtleAnimation;
 import dan200.computercraft.api.turtle.TurtleCommandResult;
-import dan200.computercraft.api.turtle.event.TurtleAction;
-import dan200.computercraft.api.turtle.event.TurtleActionEvent;
-import dan200.computercraft.shared.util.InventoryUtil;
+import dan200.computercraft.api.turtle.event.TurtleRefuelEvent;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraftforge.common.MinecraftForge;
 
 import javax.annotation.Nonnull;
 
 public class TurtleRefuelCommand implements ITurtleCommand
 {
-    private final int m_limit;
+    private final int limit;
 
     public TurtleRefuelCommand( int limit )
     {
-        m_limit = limit;
+        this.limit = limit;
     }
 
     @Nonnull
     @Override
     public TurtleCommandResult execute( @Nonnull ITurtleAccess turtle )
     {
-        if( m_limit == 0 )
+        int slot = turtle.getSelectedSlot();
+        ItemStack stack = turtle.getInventory().getStackInSlot( slot );
+        if( stack.isEmpty() ) return TurtleCommandResult.failure( "No items to combust" );
+
+        TurtleRefuelEvent event = new TurtleRefuelEvent( turtle, stack );
+        if( MinecraftForge.EVENT_BUS.post( event ) ) return TurtleCommandResult.failure( event.getFailureMessage() );
+        if( event.getHandler() == null ) return TurtleCommandResult.failure( "Items not combustible" );
+
+        if( limit != 0 )
         {
-            // If limit is zero, just check the item is combustible
-            ItemStack dummyStack = turtle.getInventory().getStackInSlot( turtle.getSelectedSlot() );
-            if( !dummyStack.isEmpty() )
-            {
-                return refuel( turtle, dummyStack, true );
-            }
-        }
-        else
-        {
-            // Otherwise, refuel for real
-            // Remove items from inventory
-            ItemStack stack = InventoryUtil.takeItems( m_limit, turtle.getItemHandler(), turtle.getSelectedSlot(), 1, turtle.getSelectedSlot() );
-            if( !stack.isEmpty() )
-            {
-                TurtleCommandResult result = refuel( turtle, stack, false );
-                if( !result.isSuccess() )
-                {
-                    // If the items weren't burnt, put them back
-                    InventoryUtil.storeItems( stack, turtle.getItemHandler(), turtle.getSelectedSlot() );
-                }
-                return result;
-            }
-        }
-        return TurtleCommandResult.failure( "No items to combust" );
-    }
-
-    private int getFuelPerItem( @Nonnull ItemStack stack )
-    {
-        return (TileEntityFurnace.getItemBurnTime( stack ) * 5) / 100;
-    }
-
-    private TurtleCommandResult refuel( ITurtleAccess turtle, @Nonnull ItemStack stack, boolean testOnly )
-    {
-        // Check if item is fuel
-        int fuelPerItem = getFuelPerItem( stack );
-        if( fuelPerItem <= 0 )
-        {
-            return TurtleCommandResult.failure( "Items not combustible" );
-        }
-
-        TurtleActionEvent event = new TurtleActionEvent( turtle, TurtleAction.REFUEL );
-        if( MinecraftForge.EVENT_BUS.post( event ) )
-        {
-            return TurtleCommandResult.failure( event.getFailureMessage() );
-        }
-
-        if( !testOnly )
-        {
-            // Determine fuel to give and replacement item to leave behind
-            int fuelToGive = fuelPerItem * stack.getCount();
-            ItemStack replacementStack = stack.getItem().getContainerItem( stack );
-
-            // Update fuel level
-            turtle.addFuel( fuelToGive );
-
-            // Store the replacement item in the inventory
-            if( !replacementStack.isEmpty() )
-            {
-                InventoryUtil.storeItems( replacementStack, turtle.getItemHandler(), turtle.getSelectedSlot() );
-            }
-
-            // Animate
+            turtle.addFuel( event.getHandler().refuel( turtle, stack, slot, limit ) );
             turtle.playAnimation( TurtleAnimation.Wait );
         }
 
