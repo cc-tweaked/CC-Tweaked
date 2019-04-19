cc-tweaked/CC-Tweaked

commit 173ea720011ea265726d434441f10708a7d798f5

Turn inspections up to 11
OK, so let's get this out of the way, there's some actual changes mixed
in here too. I'm really sorry:
 - Turtles can now not be renamed with unnamed item tags (previously it
   would clear the name, this seemed a little unideal).
 - commands.getBlock(s)Data will also include NBT.

Now, onto the horror story which is these inspection changes:
 - Make a lot of methods static
 - Typo fixes
 - Make utility classes final + private constructor
 - Lots of reformatting (ifs -> ternary, invert control flow, etc...)
 - ???
 - Profit!

I'm so going to regret this - can pretty much guarantee this is going to
break something.

src/main/java/dan200/computercraft/shared/turtle/core/TurtleCompareCommand.java
@@ -65,18 +65,18 @@ public class TurtleCompareCommand implements ITurtleCommand
                         );
                         lookAtStack = (ItemStack) method.invoke( lookAtBlock, lookAtState );
                     }
-                    catch( Exception e )
+                    catch( ReflectiveOperationException ignored )
                     {
                     }
                 }
 
                 // See if the block drops anything with the same ID as itself
                 // (try 5 times to try and beat random number generators)
-                for( int i = 0; (i < 5) && lookAtStack.isEmpty(); i++ )
+                for( int i = 0; i < 5 && lookAtStack.isEmpty(); i++ )
                 {
                     NonNullList<ItemStack> drops = NonNullList.create();
                     lookAtBlock.getDrops( drops, world, newPosition, lookAtState, 0 );
-                    if( drops.size() > 0 )
+                    if( !drops.isEmpty() )
                     {
                         for( ItemStack drop : drops )
                         {

commit 2e0ef6385d66061c80cb2c0d3336d6e3a0a7e9ad

Fix TurtleCompareCommand throwing exceptions
Originally introduced in 173ea720011ea265726d434441f10708a7d798f5, but
the underlying cause has been happening for much longer.

 - Fix order of method name parameters. Looks like this has been broken
   since 1.11. Woops.
 - Catch RuntimeExceptions too, as Forge converts checked into unchecked
   ones.

Fixes #179

src/main/java/dan200/computercraft/shared/turtle/core/TurtleCompareCommand.java
@@ -53,19 +53,15 @@ public class TurtleCompareCommand implements ITurtleCommand
             Block lookAtBlock = lookAtState.getBlock();
             if( !lookAtBlock.isAir( lookAtState, world, newPosition ) )
             {
-                // Try createStackedBlock first
+                // Try getSilkTouchDrop first
                 if( !lookAtBlock.hasTileEntity( lookAtState ) )
                 {
                     try
                     {
-                        Method method = ReflectionHelper.findMethod(
-                            Block.class,
-                            "func_180643_i", "getSilkTouchDrop",
-                            IBlockState.class
-                        );
+                        Method method = ReflectionHelper.findMethod( Block.class, "getSilkTouchDrop", "func_180643_i", IBlockState.class );
                         lookAtStack = (ItemStack) method.invoke( lookAtBlock, lookAtState );
                     }
-                    catch( ReflectiveOperationException ignored )
+                    catch( ReflectiveOperationException | RuntimeException ignored )
                     {
                     }
                 }