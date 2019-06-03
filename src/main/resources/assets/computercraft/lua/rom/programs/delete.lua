16ba46b2b3544b9c25d44c221fd30cf20602372c
JakobDev <jakobdev@gmx.de>
2019-06-03 13:19:52 +0200

    Add Check to delete.lua
    delete.lua checks now, if it has the permission to delete a file.

--- src/main/resources/assets/computercraft/lua/rom/programs/delete.lua
+++ src/main/resources/assets/computercraft/lua/rom/programs/delete.lua
@@ -9,7 +9,11 @@ for i = 1, args.n do
     local files = fs.find(shell.resolve(args[i]))
     if #files > 0 then
         for n, file in ipairs(files) do
-            fs.delete(file)
+            if fs.isReadOnly( file ) or file == "" then
+                printError( "/" .. file .. ": Access denied")
+            else
+                fs.delete(file)
+            end
         end
     else
         printError(args[i] .. ": No matching files")
