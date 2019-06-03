ffa4cc241bb3c244f408a2040558843409773369
SquidDev <bonzoweb@hotmail.co.uk>
2019-06-03 20:33:09 +0100

    Add a test utility for capturing program output
     - Make mcfly's stubbing system a little more fault-tolerant.
     - Add a small utility function which redirects print, printError and
       write to capture their output, rather than printing to the terminal.
       This can then be matched against in order to determine a program's
       output.
       It's a little flakey - you can't use it multiple times in an it
       block, etc... but it's a nice feature.
     - Add a small couple of tests to delete as a proof-of-concept.

--- src/test/resources/test-rom/spec/programs/delete_spec.lua
+++ src/test/resources/test-rom/spec/programs/delete_spec.lua
@@ -1,3 +1,5 @@
+local capture = require "test_helpers".capture_program
+
 describe("The rm program", function()
     local function touch(file)
         io.open(file, "w"):close()
@@ -31,5 +33,15 @@ describe("The rm program", function()
 
         expect(fs.exists("/test-files/a.txt")):eq(false)
         expect(fs.exists("/test-files/b.txt")):eq(false)
+    end)
+
+    it("displays the usage with no arguments", function()
+        expect(capture(stub, "rm"))
+            :matches { ok = true, output = "Usage: rm <paths>\n", error = "" }
+    end)
+
+    it("errors when a glob fails to match", function()
+        expect(capture(stub, "rm", "never-existed"))
+            :matches { ok = true, output = "", error = "never-existed: No matching files\n" }
     end)
 end)
