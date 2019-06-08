309cbdb8be2ee9a616b1fd8dd474d366987432ce
JakobDev <jakobdev@gmx.de>
2019-06-08 15:49:42 +0200

    Add `wget run` (#218)
    Equivalent to `pastebin run`, but allows running arbitrary URLs
    instead.
    
    Is this a little questionable? Yes - people shouldn't be downloading
    and running code from the internet. But hey, people do that already,
    so we might as well make it convenient.

---
+++ src/test/resources/test-rom/spec/http/wget_spec.lua
@@ -0,0 +1,53 @@
+local capture = require "test_helpers".capture_program
+
+describe("The wget program", function()
+    local function setup_request()
+        stub(_G, "http", {
+            checkURL = function()
+                return true
+            end,
+            get = function()
+                return {
+                    readAll = function()
+                        return [[print("Hello", ...)]]
+                    end,
+                    close = function()
+                    end,
+                }
+            end
+        })
+    end
+
+    it("downloads one file", function()
+        setup_request()
+        capture(stub, "wget", "https://example.com")
+
+        expect(fs.exists("/example.com")):eq(true)
+    end)
+
+    it("downloads one file with given filename", function()
+        setup_request()
+        capture(stub, "wget", "https://example.com /test-files/download")
+
+        expect(fs.exists("/test-files/download")):eq(true)
+    end)
+
+    it("runs a program from the internet", function()
+        setup_request()
+
+        expect(capture(stub, "wget", "run", "http://test.com", "a", "b", "c"))
+            :matches { ok = true, output = "Connecting to http://test.com... Success.\nHello a b c\n", error = "" }
+    end)
+
+    it("displays its usage when given no arguments", function()
+        setup_request()
+
+        expect(capture(stub, "wget"))
+            :matches { ok = true, output = "Usage:\nwget <url> [filename]\nwget run <url>\n", error = "" }
+    end)
+
+    it("can be completed", function()
+        local complete = shell.getCompletionInfo()["rom/programs/http/wget.lua"].fnComplete
+        expect(complete(shell, 1, "", {})):same { "run " }
+    end)
+end)
