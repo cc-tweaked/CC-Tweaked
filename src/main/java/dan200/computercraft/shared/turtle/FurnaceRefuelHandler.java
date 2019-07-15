4b6bddb10fa75c07877e196b7b3122ff7fd3ab84
powerboat9 <powerboat9.gamer@gmail.com>
2019-07-15 16:45:59 -0400

    Fixed fuel issue

--- src/main/java/dan200/computercraft/shared/turtle/FurnaceRefuelHandler.java
+++ src/main/java/dan200/computercraft/shared/turtle/FurnaceRefuelHandler.java
@@ -30,9 +30,14 @@ public final class FurnaceRefuelHandler implements TurtleRefuelEvent.Handler
     @Override
     public int refuel( @Nonnull ITurtleAccess turtle, @Nonnull ItemStack currentStack, int slot, int limit )
     {
-        ItemStack stack = turtle.getItemHandler().extractItem( slot, limit, false );
-        int fuelToGive = getFuelPerItem( stack ) * stack.getCount();
+        int fuelSpaceLeft = turtle.getFuelLimit() - turtle.getFuelLevel();
+        ItemStack tmpStack = turtle.getItemHandler().getStackInSlot( slot );
+        int fuelPerItem = getFuelPerItem( tmpStack );
+        int fuelItemLimit = (int) Math.ceil( fuelSpaceLeft / ( (double) fuelPerItem ) );
+        if (limit > fuelItemLimit) limit = fuelItemLimit;
 
+        ItemStack stack = turtle.getItemHandler().extractItem( slot, limit, false );
+        int fuelToGive = fuelPerItem * stack.getCount();
         // Store the replacement item in the inventory
         ItemStack replacementStack = stack.getItem().getContainerItem( stack );
         if( !replacementStack.isEmpty() )
