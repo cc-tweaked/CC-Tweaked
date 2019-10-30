38f9a015cae50e2ba1731f61fc3f6a06068c7ec4
SquidDev <bonzoweb@hotmail.co.uk>
2019-10-30 17:07:29 +0000

    Wrap all remaining uses of Grgit
    This allows you to build from a zip folder of CC:T. Fixes #307. Also fix
    the build, woops.

--- src/test/java/dan200/computercraft/core/computer/BasicEnvironment.java
+++ src/test/java/dan200/computercraft/core/computer/BasicEnvironment.java
@@ -23,7 +23,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 /**
- * A very basic environment
+ * A very basic environment.
  */
 public class BasicEnvironment implements IComputerEnvironment
 {
@@ -92,7 +92,6 @@ public class BasicEnvironment implements IComputerEnvironment
     {
         return ComputerCraft.class.getClassLoader().getResourceAsStream( "assets/" + domain + "/" + subPath );
     }
-
 
     public static IMount createMount( Class<?> klass, String path, String fallback )
     {
