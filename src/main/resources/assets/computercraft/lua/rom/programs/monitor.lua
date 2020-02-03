074962cb752759161aa9a34c5c86064bfd570f46
Fatboychummy <39929480+fatboychummy@users.noreply.github.com>
2020-02-03 14:40:26 -0700

    some minor optimization

--- src/main/resources/assets/computercraft/lua/rom/programs/monitor.lua
+++ src/main/resources/assets/computercraft/lua/rom/programs/monitor.lua
@@ -61,9 +61,8 @@ local ok, param = pcall( function()
         end
         if coroutine.status( co ) ~= "dead" and (sFilter == nil or sFilter == "mouse_up") then
             if tEvent[1] == "timer" and timers[tEvent[2]] then
-                local x, y = table.unpack(timers[tEvent[2]], 1, 2)
+                sFilter = resume( "mouse_up", 1, table.unpack( timers[tEvent[2]], 1, 2 ) )
                 timers[tEvent[2]] = nil
-                sFilter = resume( "mouse_up", 1, x, y )
             end
         end
     end
