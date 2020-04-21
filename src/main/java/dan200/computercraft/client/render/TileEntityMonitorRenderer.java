457151eefa935276be278cf06ede75876aaccd8d
SquidDev <git@squiddev.cc>
2020-04-21 10:29:00 +0100

    Revert "Basic culling of monitors"
    This reverts commit 4d3cce467ed58a1230ec6897f500250140a0f492.
    
    This would be really nice to have, but is sadly much trickier to
    implement than I initially anticipated. Minecraft doesn't provide access
    to the _actual_ camera, and given the odd things we need to take into
    account (third person, "hurt" camera roll, etc...) I'm not going to
    bother right now.
    
    I'll look into this again at some point in the future - I'm 90% sure
    it's possible, and if not on 1.12, then probably on 1.1{4,5}. Goodness
    knows :).

--- src/main/java/dan200/computercraft/client/render/TileEntityMonitorRenderer.java
+++ src/main/java/dan200/computercraft/client/render/TileEntityMonitorRenderer.java
@@ -19,10 +19,8 @@ import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.client.renderer.vertex.VertexBuffer;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import org.lwjgl.opengl.GL11;
 
 import javax.annotation.Nonnull;
@@ -60,14 +58,6 @@ public class TileEntityMonitorRenderer extends TileEntitySpecialRenderer<TileMon
         originTerminal.lastRenderFrame = renderFrame;
         originTerminal.lastRenderPos = monitorPos;
 
-        Minecraft mc = Minecraft.getMinecraft();
-
-        Entity entity = mc.getRenderViewEntity();
-        if( entity != null && new Vec3d( origin.getFront().getDirectionVec() ).dotProduct( entity.getLookVec() ) > 0 )
-        {
-            return;
-        }
-
         BlockPos originPos = origin.getPos();
         posX += originPos.getX() - monitorPos.getX();
         posY += originPos.getY() - monitorPos.getY();
@@ -92,6 +82,9 @@ public class TileEntityMonitorRenderer extends TileEntitySpecialRenderer<TileMon
         );
         double xSize = origin.getWidth() - 2.0 * (RENDER_MARGIN + TileMonitor.RENDER_BORDER);
         double ySize = origin.getHeight() - 2.0 * (RENDER_MARGIN + TileMonitor.RENDER_BORDER);
+
+        // Get renderers
+        Minecraft mc = Minecraft.getMinecraft();
 
         // Set up render state for monitors. We disable writing to the depth buffer (we draw a "blocker" later),
         // and setup lighting so that we render with a glow.
