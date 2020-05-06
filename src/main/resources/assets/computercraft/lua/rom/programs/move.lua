94c821f0ca21326e2510cb2287110c1d553b9936
Lupus590 <lupussolitarius590@gmail.com>
2020-05-06 21:38:24 +0100

    move improvements

--- src/main/resources/assets/computercraft/lua/rom/programs/move.lua
+++ src/main/resources/assets/computercraft/lua/rom/programs/move.lua
@@ -8,11 +8,31 @@ end
 local sSource = shell.resolve(tArgs[1])
 local sDest = shell.resolve(tArgs[2])
 local tFiles = fs.find(sSource)
+
+local function sanityChecks(sSource, sDestination)
+    if fs.isMountPoint(sSource) then
+        printError("Can't move mounts")
+        return false
+    elseif fs.isReadOnly(sSource) then
+        printError("Source is read-only")
+        return false
+    elseif fs.exists(sDest) then
+        printError("Destination exists")
+        return false
+    elseif fs.isReadOnly(sDest) then
+        printError("Destination is read-only")
+        return false
+    end
+    return true
+end
+
 if #tFiles > 0 then
     for _, sFile in ipairs(tFiles) do
         if fs.isDir(sDest) then
+            if not sanityChecks(sFile, sDest) then return end
             fs.move(sFile, fs.combine(sDest, fs.getName(sFile)))
         elseif #tFiles == 1 then
+            if not sanityChecks(sFile, sDest) then return end
             fs.move(sFile, sDest)
         else
             printError("Cannot overwrite file multiple times")
--- src/test/resources/test-rom/spec/programs/move_spec.lua
+++ src/test/resources/test-rom/spec/programs/move_spec.lua
@@ -14,9 +14,33 @@ describe("The move program", function()
         expect(fs.exists("/test-files/move/b.txt")):eq(true)
     end)
 
-    it("try to move a not existing file", function()
+    it("fails when moving a file which doesn't exist", function()
         expect(capture(stub, "move nothing destination"))
             :matches { ok = true, output = "", error = "No matching files\n" }
+    end)
+
+    it("fails when overwriting an existing file", function()
+        touch("/test-files/move/c.txt")
+
+        expect(capture(stub, "move /test-files/move/c.txt /test-files/move/c.txt"))
+            :matches { ok = true, output = "", error = "Destination exists\n" }
+    end)
+
+    it("fails when moving to read-only locations", function()
+        touch("/test-files/move/d.txt")
+
+        expect(capture(stub, "move /test-files/move/d.txt /rom/test.txt"))
+            :matches { ok = true, output = "", error = "Destination is read-only\n" }
+    end)
+
+    it("fails when moving from read-only locations", function()
+        expect(capture(stub, "move /rom/startup.lua /test-files/move/d.txt"))
+            :matches { ok = true, output = "", error = "Source is read-only\n" }
+    end)
+
+    it("fails when moving mounts", function()
+        expect(capture(stub, "move /rom /test-files/move/rom"))
+            :matches { ok = true, output = "", error = "Can't move mounts\n" }
     end)
 
     it("displays the usage with no arguments", function()
