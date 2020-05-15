161a5b47077b3e3dfb9652cca55c5431a127735c
SquidDev <git@squiddev.cc>
2020-05-15 10:03:47 +0100

    Document and test the redstone library
    The tests may be a little agressive, but I wanted some sanity checks for
    the 1.15 API rewrite.

--- src/main/java/dan200/computercraft/core/apis/RedstoneAPI.java
+++ src/main/java/dan200/computercraft/core/apis/RedstoneAPI.java
@@ -16,11 +16,11 @@ import static dan200.computercraft.api.lua.ArgumentHelper.*;
 
 public class RedstoneAPI implements ILuaAPI
 {
-    private IAPIEnvironment m_environment;
+    private final IAPIEnvironment environment;
 
     public RedstoneAPI( IAPIEnvironment environment )
     {
-        m_environment = environment;
+        this.environment = environment;
     }
 
     @Override
@@ -63,31 +63,31 @@ public class RedstoneAPI implements ILuaAPI
                 // setOutput
                 ComputerSide side = parseSide( args );
                 boolean output = getBoolean( args, 1 );
-                m_environment.setOutput( side, output ? 15 : 0 );
+                environment.setOutput( side, output ? 15 : 0 );
                 return null;
             }
             case 2: // getOutput
-                return new Object[] { m_environment.getOutput( parseSide( args ) ) > 0 };
+                return new Object[] { environment.getOutput( parseSide( args ) ) > 0 };
             case 3: // getInput
-                return new Object[] { m_environment.getInput( parseSide( args ) ) > 0 };
+                return new Object[] { environment.getInput( parseSide( args ) ) > 0 };
             case 4:
             {
                 // setBundledOutput
                 ComputerSide side = parseSide( args );
                 int output = getInt( args, 1 );
-                m_environment.setBundledOutput( side, output );
+                environment.setBundledOutput( side, output );
                 return null;
             }
             case 5: // getBundledOutput
-                return new Object[] { m_environment.getBundledOutput( parseSide( args ) ) };
+                return new Object[] { environment.getBundledOutput( parseSide( args ) ) };
             case 6: // getBundledInput
-                return new Object[] { m_environment.getBundledInput( parseSide( args ) ) };
+                return new Object[] { environment.getBundledInput( parseSide( args ) ) };
             case 7:
             {
                 // testBundledInput
                 ComputerSide side = parseSide( args );
                 int mask = getInt( args, 1 );
-                int input = m_environment.getBundledInput( side );
+                int input = environment.getBundledInput( side );
                 return new Object[] { (input & mask) == mask };
             }
             case 8:
@@ -100,15 +100,15 @@ public class RedstoneAPI implements ILuaAPI
                 {
                     throw new LuaException( "Expected number in range 0-15" );
                 }
-                m_environment.setOutput( side, output );
+                environment.setOutput( side, output );
                 return null;
             }
             case 10:
             case 11: // getAnalogOutput/getAnalogueOutput
-                return new Object[] { m_environment.getOutput( parseSide( args ) ) };
+                return new Object[] { environment.getOutput( parseSide( args ) ) };
             case 12:
             case 13: // getAnalogInput/getAnalogueInput
-                return new Object[] { m_environment.getInput( parseSide( args ) ) };
+                return new Object[] { environment.getInput( parseSide( args ) ) };
             default:
                 return null;
         }
