ac2e544a28e5aee8f56a053d2a275af17e45c479
SquidDev <git@squiddev.cc>
2020-06-23 18:41:15 +0100

    Add several tests
    Also fix a couple of bugs:
     - Incorrect argument validation for draw redirect target.
     - Empty lines were being skipped from the output.

--- src/main/resources/data/computercraft/lua/rom/modules/main/cc/image/nft.lua
+++ src/main/resources/data/computercraft/lua/rom/modules/main/cc/image/nft.lua
@@ -27,8 +27,6 @@ local function parse(image)
 
     local i, len = 1, #image
     while i <= len do
-        print(i)
-
         local c = image:sub(i, i)
         if c == "\31" and i < len then
             i = i + 1
@@ -37,10 +35,14 @@ local function parse(image)
             i = i + 1
             background = image:sub(i, i)
         elseif c == "\n" then
+            if result[line] == nil then
+                result[line] = { text = "", foreground = "", background = "" }
+            end
+
             line = line + 1
         else
             local next = image:find("[\n\30\31]", i) or #image + 1
-            local len = next - i
+            local seg_len = next - i
 
             local this_line = result[line]
             if this_line == nil then
@@ -49,8 +51,8 @@ local function parse(image)
             end
 
             this_line.text = this_line.text .. image:sub(i, next - 1)
-            this_line.foreground = this_line.foreground .. foreground:rep(len)
-            this_line.background = this_line.background .. background:rep(len)
+            this_line.foreground = this_line.foreground .. foreground:rep(seg_len)
+            this_line.background = this_line.background .. background:rep(seg_len)
 
             i = next - 1
         end
@@ -88,7 +90,7 @@ local function draw(image, xPos, yPos, target)
     expect(1, image, "table")
     expect(2, xPos, "number")
     expect(3, yPos, "number")
-    expect(4, target, "table", nil)
+    expect(4, target, "table", "nil")
 
     if not target then target = term end
 
