a5f7cf833400377fb4cf6464a6b04d8a0f50615e
Naheulf <newheulf@gmail.com>
2020-07-06 15:18:07 +0200

    Add enchantments and unbreakable to ItemData.java. (#488)

--- src/main/java/dan200/computercraft/shared/peripheral/generic/data/ItemData.java
+++ src/main/java/dan200/computercraft/shared/peripheral/generic/data/ItemData.java
@@ -7,6 +7,9 @@
 package dan200.computercraft.shared.peripheral.generic.data;
 
 import com.google.gson.JsonParseException;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.INBT;
@@ -16,8 +19,7 @@ import net.minecraftforge.common.util.Constants;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ItemData
@@ -25,7 +27,7 @@ public class ItemData
     @Nonnull
     public static <T extends Map<? super String, Object>> T fillBasic( @Nonnull T data, @Nonnull ItemStack stack )
     {
-        data.put( "name", Objects.toString( stack.getItem().getRegistryName() ) );
+        data.put( "name", DataHelpers.getId( stack.getItem() ) );
         data.put( "count", stack.getCount() );
         return data;
     }
@@ -68,6 +70,21 @@ public class ItemData
             }
         }
 
+        /*
+         * Used to hide some data from ItemStack tooltip.
+         * @see https://minecraft.gamepedia.com/Tutorials/Command_NBT_tags
+         * @see ItemStack#getTooltip
+         */
+        int hideFlags = tag != null ? tag.getInt( "HideFlags" ) : 0;
+
+        List<Map<String, Object>> enchants = getAllEnchants( stack, hideFlags );
+        if( !enchants.isEmpty() ) data.put( "enchantments", enchants );
+
+        if( tag != null && tag.getBoolean( "Unbreakable" ) && (hideFlags & 4) == 0 )
+        {
+            data.put( "unbreakable", true );
+        }
+
         return data;
     }
 
@@ -81,6 +98,61 @@ public class ItemData
         catch( JsonParseException e )
         {
             return null;
+        }
+    }
+
+    /**
+     * Retrieve all visible enchantments from given stack. Try to follow all tooltip rules : order and visibility.
+     *
+     * @param stack     Stack to analyse
+     * @param hideFlags An int used as bit field to provide visibility rules.
+     * @return A filled list that contain all visible enchantments.
+     */
+    @Nonnull
+    private static List<Map<String, Object>> getAllEnchants( @Nonnull ItemStack stack, int hideFlags )
+    {
+        ArrayList<Map<String, Object>> enchants = new ArrayList<>( 0 );
+
+        if( stack.getItem() instanceof EnchantedBookItem && (hideFlags & 32) == 0 )
+        {
+            addEnchantments( EnchantedBookItem.getEnchantments( stack ), enchants );
+        }
+
+        if( stack.isEnchanted() && (hideFlags & 1) == 0 )
+        {
+            /*
+             * Mimic the EnchantmentHelper.getEnchantments(ItemStack stack) behavior without special case for Enchanted book.
+             * I'll do that to have the same data than ones displayed in tooltip.
+             * @see EnchantmentHelper.getEnchantments(ItemStack stack)
+             */
+            addEnchantments( stack.getEnchantmentTagList(), enchants );
+        }
+
+        return enchants;
+    }
+
+    /**
+     * Converts a Mojang enchant map to a Lua list.
+     *
+     * @param rawEnchants The raw NBT list of enchantments
+     * @param enchants    The enchantment map to add it to.
+     * @see net.minecraft.enchantment.EnchantmentHelper
+     */
+    private static void addEnchantments( @Nonnull ListNBT rawEnchants, @Nonnull ArrayList<Map<String, Object>> enchants )
+    {
+        if( rawEnchants.isEmpty() ) return;
+
+        enchants.ensureCapacity( enchants.size() + rawEnchants.size() );
+
+        for( Map.Entry<Enchantment, Integer> entry : EnchantmentHelper.func_226652_a_( rawEnchants ).entrySet() )
+        {
+            Enchantment enchantment = entry.getKey();
+            Integer level = entry.getValue();
+            HashMap<String, Object> enchant = new HashMap<>( 3 );
+            enchant.put( "name", DataHelpers.getId( enchantment ) );
+            enchant.put( "level", level );
+            enchant.put( "displayName", enchantment.getDisplayName( level ).getString() );
+            enchants.add( enchant );
         }
     }
 }
