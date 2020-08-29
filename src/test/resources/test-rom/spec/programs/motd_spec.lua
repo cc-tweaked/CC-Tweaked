592c5d781694bc33030acef0d7620cdf10c8b02c
JackMacWindows <jackmacwindowslinux@gmail.com>
2020-08-29 03:57:30 -0400

    Fixed whitespace

--- src/test/resources/test-rom/spec/programs/motd_spec.lua
+++ src/test/resources/test-rom/spec/programs/motd_spec.lua
@@ -1,7 +1,6 @@
 local capture = require "test_helpers".capture_program
 
 describe("The motd program", function()
-        
     local function setup_date(month, day)
         stub(_G, "os", {
             date = function() return { month = month, day = day } end,
@@ -22,19 +21,19 @@ describe("The motd program", function()
         expect(capture(stub, "motd"))
             :matches { ok = true, output = "Hello World!\n", error = "" }
     end)
-        
+
     it("displays date-specific MOTD (1/1)", function()
         setup_date(1, 1)
         expect(capture(stub, "motd"))
             :matches { ok = true, output = "Happy new year!\n", error = "" }
     end)
-        
+
     it("displays date-specific MOTD (10/31)", function()
         setup_date(10, 31)
         expect(capture(stub, "motd"))
             :matches { ok = true, output = "OOoooOOOoooo! Spooky!\n", error = "" }
     end)
-        
+
     it("displays date-specific MOTD (12/24)", function()
         setup_date(12, 24)
         expect(capture(stub, "motd"))
