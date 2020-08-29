0c4c6cbb696a2cb9afd5572bc0a458cffdc14c8e
JackMacWindows <jackmacwindowslinux@gmail.com>
2020-08-29 03:33:31 -0400

    Added tests

--- src/test/resources/test-rom/spec/programs/motd_spec.lua
+++ src/test/resources/test-rom/spec/programs/motd_spec.lua
@@ -1,14 +1,33 @@
 local capture = require "test_helpers".capture_program
 
 describe("The motd program", function()
+        
+    local function setup_date(month, day)
+        stub(_G, "os", { date = function() return { month = month, day = day } end })
+    end
 
-    it("displays MODT", function()
-        local file = fs.open("/modt_check.txt", "w")
+    it("displays MOTD", function()
+        setup_date(0, 0)
+        local file = fs.open("/motd_check.txt", "w")
         file.write("Hello World!")
         file.close()
-        settings.set("motd.path", "/modt_check.txt")
+        settings.set("motd.path", "/motd_check.txt")
 
         expect(capture(stub, "motd"))
             :matches { ok = true, output = "Hello World!\n", error = "" }
+    end)
+        
+    it("displays date-specific MOTD", function()
+        setup_date(1, 1)
+        expect(capture(stub, "motd"))
+            :matches { ok = true, output = "Happy new year!\n", error = "" }
+                
+        setup_date(10, 31)
+        expect(capture(stub, "motd"))
+            :matches { ok = true, output = "OOoooOOOoooo! Spooky!\n", error = "" }
+        
+        setup_date(12, 24)
+        expect(capture(stub, "motd"))
+            :matches { ok = true, output = "Merry X-mas!\n", error = "" }
     end)
 end)
