650ad4e151fe73c7bde4237f5da7f42cf8875677
JackMacWindows <jackmacwindowslinux@gmail.com>
2020-08-29 03:50:32 -0400

    Fixed CI

--- src/test/resources/test-rom/spec/programs/motd_spec.lua
+++ src/test/resources/test-rom/spec/programs/motd_spec.lua
@@ -3,7 +3,13 @@ local capture = require "test_helpers".capture_program
 describe("The motd program", function()
         
     local function setup_date(month, day)
-        os.date = function() return { month = month, day = day } end
+        stub(_G, "os", {
+            date = function() return { month = month, day = day } end,
+            run = os.run,
+            queueEvent = os.queueEvent,
+            pullEvent = os.pullEvent,
+            startTimer = os.startTimer
+        })
     end
 
     it("displays MOTD", function()
