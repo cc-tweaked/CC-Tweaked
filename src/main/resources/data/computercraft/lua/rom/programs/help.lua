a1d91939380f43e882e730b5a1c38d02a137ea79
JackMacWindows <jackmacwindowslinux@gmail.com>
2020-12-01 17:47:11 -0500

    Replaced -/* with bullet points, fixed small files

--- src/main/resources/data/computercraft/lua/rom/programs/help.lua
+++ src/main/resources/data/computercraft/lua/rom/programs/help.lua
@@ -21,39 +21,43 @@ if file then
 
     local w, h = term.getSize()
     local oldterm = term.redirect(window.create(term.current(), 1, 1, w, h, false))
-    local len = print(sContents)
+    local len = print(sContents:gsub("[-*]", "\7"))
     term.redirect(oldterm)
     local win = window.create(term.current(), 1, 1, w, len)
+    term.clear()
     oldterm = term.redirect(win)
     write(sContents)
     local yPos = 1
     while true do
         local ev = { os.pullEvent() }
         if ev[1] == "key" then
-           if ev[2] == keys.up and yPos < 1 then
-               yPos = yPos + 1
-               win.reposition(1, yPos)
-           elseif ev[2] == keys.down and yPos > -len + h + 1 then
-               yPos = yPos - 1
-               win.reposition(1, yPos)
-            elseif ev[2] == keys.pageUp and yPos < 1 then
-                yPos = math.min(yPos + h, 1)
-                win.reposition(1, yPos)
-            elseif ev[2] == keys.pageDown and yPos > -len + h + 1 then
-                yPos = math.max(yPos - h, -len + h + 1)
-               win.reposition(1, yPos)
-           elseif ev[2] == keys.home then
-                yPos = 1
-               win.reposition(1, yPos)
-            elseif ev[2] == keys["end"] then
-                yPos = -len + h + 1
-                win.reposition(1, yPos)
-           elseif ev[2] == keys.q then break end
-       elseif ev[1] == "mouse_scroll" then
-           if ev[2] == -1 and yPos < 1 then
+            if len > h then
+                if ev[2] == keys.up and yPos < 1 then
+                    yPos = yPos + 1
+                    win.reposition(1, yPos)
+                elseif ev[2] == keys.down and yPos > -len + h + 1 then
+                    yPos = yPos - 1
+                    win.reposition(1, yPos)
+                elseif ev[2] == keys.pageUp and yPos < 1 then
+                    yPos = math.min(yPos + h, 1)
+                    win.reposition(1, yPos)
+                elseif ev[2] == keys.pageDown and yPos > -len + h + 1 then
+                    yPos = math.max(yPos - h, -len + h + 1)
+                    win.reposition(1, yPos)
+                elseif ev[2] == keys.home then
+                    yPos = 1
+                    win.reposition(1, yPos)
+                elseif ev[2] == keys["end"] then
+                    yPos = -len + h + 1
+                    win.reposition(1, yPos)
+                end
+            end
+            if ev[2] == keys.q then break end
+       elseif ev[1] == "mouse_scroll" and len > h then
+            if ev[2] == -1 and yPos < 1 then
                 yPos = yPos + 1
                 win.reposition(1, yPos)
-           elseif ev[2] == 1 and yPos > -len + h + 1 then
+            elseif ev[2] == 1 and yPos > -len + h + 1 then
                 yPos = yPos - 1
                 win.reposition(1, yPos)
             end
