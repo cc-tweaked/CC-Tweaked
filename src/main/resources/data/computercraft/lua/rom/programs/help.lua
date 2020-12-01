e6a1f88062a8392bcdd4699dc03cff7f2a79f4e5
JackMacWindows <jackmacwindowslinux@gmail.com>
2020-12-01 17:38:52 -0500

    Added improved help viewer

--- src/main/resources/data/computercraft/lua/rom/programs/help.lua
+++ src/main/resources/data/computercraft/lua/rom/programs/help.lua
@@ -19,8 +19,49 @@ if file then
     local sContents = file:read("*a")
     file:close()
 
-    local _, nHeight = term.getSize()
-    textutils.pagedPrint(sContents, nHeight - 3)
+    local w, h = term.getSize()
+    local oldterm = term.redirect(window.create(term.current(), 1, 1, w, h, false))
+    local len = print(sContents)
+    term.redirect(oldterm)
+    local win = window.create(term.current(), 1, 1, w, len)
+    oldterm = term.redirect(win)
+    write(sContents)
+    local yPos = 1
+    while true do
+        local ev = { os.pullEvent() }
+        if ev[1] == "key" then
+           if ev[2] == keys.up and yPos < 1 then
+               yPos = yPos + 1
+               win.reposition(1, yPos)
+           elseif ev[2] == keys.down and yPos > -len + h + 1 then
+               yPos = yPos - 1
+               win.reposition(1, yPos)
+            elseif ev[2] == keys.pageUp and yPos < 1 then
+                yPos = math.min(yPos + h, 1)
+                win.reposition(1, yPos)
+            elseif ev[2] == keys.pageDown and yPos > -len + h + 1 then
+                yPos = math.max(yPos - h, -len + h + 1)
+               win.reposition(1, yPos)
+           elseif ev[2] == keys.home then
+                yPos = 1
+               win.reposition(1, yPos)
+            elseif ev[2] == keys["end"] then
+                yPos = -len + h + 1
+                win.reposition(1, yPos)
+           elseif ev[2] == keys.q then break end
+       elseif ev[1] == "mouse_scroll" then
+           if ev[2] == -1 and yPos < 1 then
+                yPos = yPos + 1
+                win.reposition(1, yPos)
+           elseif ev[2] == 1 and yPos > -len + h + 1 then
+                yPos = yPos - 1
+                win.reposition(1, yPos)
+            end
+        end
+    end
+    term.redirect(oldterm)
+    term.setCursorPos(1, 1)
+    term.clear()
 else
     print("No help available")
 end
