24d3777722812f975d2bc4594437fbbb0431d910
JackMacWindows <jackmacwindowslinux@gmail.com>
2020-12-02 14:22:12 -0500

    Added improved help viewer (#595)
     - Pagination, with (page) up/down, q(uit) and scrolling support.
     - Render markdown style bullets ('-'/'*') using a '•' instead.

--- src/test/resources/test-rom/spec/programs/help_spec.lua
+++ src/test/resources/test-rom/spec/programs/help_spec.lua
@@ -3,6 +3,6 @@ local capture = require "test_helpers".capture_program
 describe("The help program", function()
     it("errors when there is no such help file", function()
         expect(capture(stub, "help nothing"))
-            :matches { ok = true, output = "No help available\n", error = "" }
+            :matches { ok = true, error = "No help available\n", output = "" }
     end)
 end)
