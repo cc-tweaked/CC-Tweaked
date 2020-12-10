4acc7986d4cb9cf6a23f7b795f2f30d12e784d3c
SquidDev <git@squiddev.cc>
2020-12-10 18:32:47 +0000

    Roll our own wrapping function for help
    This saves us writing to a buffer multiple times, and so makes things
    much, much faster.

--- src/main/resources/data/computercraft/lua/rom/programs/help.lua
+++ src/main/resources/data/computercraft/lua/rom/programs/help.lua
@@ -13,6 +13,61 @@ if sTopic == "index" then
     return
 end
 
+local function word_wrap(text, width)
+    local lines, lines_n, current_line = {}, 0, ""
+    local function push_line()
+        lines_n = lines_n + 1
+        lines[lines_n] = current_line
+        current_line = ""
+    end
+
+    local pos, length = 1, #text
+    local sub, match = string.sub, string.match
+    while pos <= length do
+        local head = sub(text, pos, pos)
+        if head == " " or head == "\t" then
+            local whitespace = match(text, "^[ \t]+", pos)
+            current_line = current_line .. whitespace
+            pos = pos + #whitespace
+        elseif head == "\n" then
+            push_line()
+            pos = pos + 1
+        else
+            local word = match(text, "^[^ \t\n]+", pos)
+            pos = pos + #word
+            if #word > width then
+                -- Print a multiline word
+                while #word > 0 do
+                    local space_remaining = width - #current_line - 1
+                    if space_remaining <= 0 then
+                        push_line()
+                        space_remaining = width
+                    end
+
+                    current_line = current_line .. sub(word, 1, space_remaining)
+                    word = sub(word, space_remaining + 1)
+                end
+            else
+                -- Print a word normally
+                if width - #current_line - 1 < #word then push_line() end
+                current_line = current_line .. word
+            end
+        end
+    end
+
+    push_line()
+
+    -- Normalise the strings suitable for use with blit. We could skip this and
+    -- just use term.write, but saves us a clearLine call.
+    for k, line in pairs(lines) do
+        line = line:sub(1, width)
+        if #line < width then line = line .. (" "):rep(width - #line) end
+        lines[k] = line
+    end
+
+    return lines
+end
+
 local sFile = help.lookup(sTopic)
 local file = sFile ~= nil and io.open(sFile) or nil
 if not file then
@@ -24,37 +79,27 @@ local contents = file:read("*a"):gsub("(\n *)[-*]( +)", "%1\7%2")
 file:close()
 
 local width, height = term.getSize()
-local buffer = window.create(term.current(), 1, 1, width, height, false)
-local old_term = term.redirect(buffer)
-
-local print_height = print(contents) + 1
+local lines = word_wrap(contents, width)
+local print_height = #lines
 
 -- If we fit within the screen, just display without pagination.
 if print_height <= height then
-    term.redirect(old_term)
     print(contents)
     return
-end
-
-local function draw_buffer(width)
-    buffer.reposition(1, 1, width, print_height)
-    buffer.clear()
-    buffer.setCursorPos(1, 1)
-    print(contents)
-    term.redirect(old_term)
 end
 
 local offset = 0
 
 local function draw()
+    local fg, bg = ("0"):rep(width), ("f"):rep(width)
     for y = 1, height - 1 do
         term.setCursorPos(1, y)
         if y + offset > print_height then
-             -- Should only happen if we resize the terminal to a larger one
-             -- than actually needed for the current text.
+            -- Should only happen if we resize the terminal to a larger one
+            -- than actually needed for the current text.
             term.clearLine()
         else
-            term.blit(buffer.getLine(y + offset))
+            term.blit(lines[y + offset], fg, bg)
         end
     end
 end
@@ -73,7 +118,6 @@ local function draw_menu()
     end
 end
 
-draw_buffer(width)
 draw()
 draw_menu()
 
@@ -114,11 +158,8 @@ while true do
         local new_width, new_height = term.getSize()
 
         if new_width ~= width then
-            buffer.setCursorPos(1, 1)
-            buffer.reposition(1, 1, new_width, print_height)
-            term.redirect(buffer)
-            print_height = print(contents) + 1
-            draw_buffer(new_width)
+            lines = word_wrap(contents, new_width)
+            print_height = #lines
         end
 
         width, height = new_width, new_height
@@ -128,6 +169,5 @@ while true do
     end
 end
 
-term.redirect(old_term)
 term.setCursorPos(1, 1)
 term.clear()
