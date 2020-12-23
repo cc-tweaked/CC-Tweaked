150c492991a03d59f4e0e12d278c7fccf29e9364
Jonathan Coates <git@squiddev.cc>
2020-12-23 16:27:04 +0000

    Use cc.strings instead

--- src/main/resources/data/computercraft/lua/rom/programs/help.lua
+++ src/main/resources/data/computercraft/lua/rom/programs/help.lua
@@ -13,56 +13,14 @@ if sTopic == "index" then
     return
 end
 
+local strings = require "cc.strings"
 local function word_wrap(text, width)
-    local lines, lines_n, current_line = {}, 0, ""
-    local function push_line()
-        lines_n = lines_n + 1
-        lines[lines_n] = current_line
-        current_line = ""
-    end
-
-    local pos, length = 1, #text
-    local sub, match = string.sub, string.match
-    while pos <= length do
-        local head = sub(text, pos, pos)
-        if head == " " or head == "\t" then
-            local whitespace = match(text, "^[ \t]+", pos)
-            current_line = current_line .. whitespace
-            pos = pos + #whitespace
-        elseif head == "\n" then
-            push_line()
-            pos = pos + 1
-        else
-            local word = match(text, "^[^ \t\n]+", pos)
-            pos = pos + #word
-            if #word > width then
-                -- Print a multiline word
-                while #word > 0 do
-                    local space_remaining = width - #current_line - 1
-                    if space_remaining <= 0 then
-                        push_line()
-                        space_remaining = width
-                    end
-
-                    current_line = current_line .. sub(word, 1, space_remaining)
-                    word = sub(word, space_remaining + 1)
-                end
-            else
-                -- Print a word normally
-                if width - #current_line - 1 < #word then push_line() end
-                current_line = current_line .. word
-            end
-        end
-    end
-
-    push_line()
+    local lines = strings.wrap(text, width)
 
     -- Normalise the strings suitable for use with blit. We could skip this and
     -- just use term.write, but saves us a clearLine call.
     for k, line in pairs(lines) do
-        line = line:sub(1, width)
-        if #line < width then line = line .. (" "):rep(width - #line) end
-        lines[k] = line
+        lines[k] = strings.ensure_width(line, width)
     end
 
     return lines
