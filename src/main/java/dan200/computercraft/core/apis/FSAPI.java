814d8c6ee979a821e3039e7bb65fbcf7591f967c
Lupus590 <lupussolitarius590@gmail.com>
2021-01-06 21:15:11 +0000

    add docs

--- src/main/java/dan200/computercraft/core/apis/FSAPI.java
+++ src/main/java/dan200/computercraft/core/apis/FSAPI.java
@@ -471,8 +471,8 @@ public class FSAPI implements ILuaAPI
     /**
      * Get attributes about a specific file or folder.
      *
-     * The returned attributes table contains information about the size of the file, whether it is a directory, and
-     * when it was created and last modified.
+     * The returned attributes table contains information about the size of the file, whether it is a directory,
+     * when it was created and last modified, and  whether it is read only.
      *
      * The creation and modification times are given as the number of milliseconds since the UNIX epoch. This may be
      * given to {@link OSAPI#date} in order to convert it to more usable form.
@@ -480,7 +480,7 @@ public class FSAPI implements ILuaAPI
      * @param path The path to get attributes for.
      * @return The resulting attributes.
      * @throws LuaException If the path does not exist.
-     * @cc.treturn { size = number, isDir = boolean, created = number, modified = number } The resulting attributes.
+     * @cc.treturn { size = number, isDir = boolean, isReadOnly = boolean, created = number, modified = number } The resulting attributes.
      * @see #getSize If you only care about the file's size.
      * @see #isDir If you only care whether a path is a directory or not.
      */
