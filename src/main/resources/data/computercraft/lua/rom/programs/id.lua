459baac73640ea75d28b1607dfac5bfd91983046
Wojbie <Wojbie@gmail.com>
2021-01-13 18:23:06 +0100

    Fix/Update id.lua
    Resolves concatenate nil error with disk.getID(sDrive) for non-disk medium. Adds identification for audio and non-disk medium.

--- src/main/resources/data/computercraft/lua/rom/programs/id.lua
+++ src/main/resources/data/computercraft/lua/rom/programs/id.lua
@@ -13,16 +13,32 @@ if sDrive == nil then
     end
 
 else
+    local bAudio = disk.hasAudio(sDrive)
+    if bAudio then
+        print("Audio medium")
+
+        local title = disk.getAudioTitle(sDrive)
+        if title then
+            print("Audio is titled \"" .. title .. "\"")
+        end
+        return
+    end
+
     local bData = disk.hasData(sDrive)
     if not bData then
         print("No disk in drive " .. sDrive)
         return
     end
 
-    print("The disk is #" .. disk.getID(sDrive))
+    local id = disk.getID(sDrive)
+    if id then
+        print("The disk is #" .. disk.getID(sDrive))
+    else
+        print("Non-disk data medium")
+    end
 
     local label = disk.getLabel(sDrive)
     if label then
-        print("The disk is labelled \"" .. label .. "\"")
+        print("Labelled \"" .. label .. "\"")
     end
 end
