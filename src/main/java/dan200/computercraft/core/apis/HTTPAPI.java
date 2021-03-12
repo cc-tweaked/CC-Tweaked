3860e2466cb9083efe77504724a7b4955c1d75cf
Jonathan Coates <git@squiddev.cc>
2021-03-12 09:14:52 +0000

    Add User-Agent to Websockets
    I think, haven't actually tested this :D:. Closes #730.

--- src/main/java/dan200/computercraft/core/apis/HTTPAPI.java
+++ src/main/java/dan200/computercraft/core/apis/HTTPAPI.java
@@ -3,37 +3,29 @@
  * Copyright Daniel Ratcliffe, 2011-2021. Do not distribute without permission.
  * Send enquiries to dratcliffe@gmail.com
  */
-
 package dan200.computercraft.core.apis;
-
-import static dan200.computercraft.core.apis.TableHelper.getStringField;
-import static dan200.computercraft.core.apis.TableHelper.optBooleanField;
-import static dan200.computercraft.core.apis.TableHelper.optStringField;
-import static dan200.computercraft.core.apis.TableHelper.optTableField;
-
-import java.net.URI;
-import java.util.Collections;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Optional;
-
-import javax.annotation.Nonnull;
 
 import dan200.computercraft.ComputerCraft;
 import dan200.computercraft.api.lua.IArguments;
 import dan200.computercraft.api.lua.ILuaAPI;
 import dan200.computercraft.api.lua.LuaException;
 import dan200.computercraft.api.lua.LuaFunction;
-import dan200.computercraft.core.apis.http.CheckUrl;
-import dan200.computercraft.core.apis.http.HTTPRequestException;
-import dan200.computercraft.core.apis.http.Resource;
-import dan200.computercraft.core.apis.http.ResourceGroup;
-import dan200.computercraft.core.apis.http.ResourceQueue;
+import dan200.computercraft.core.apis.http.*;
 import dan200.computercraft.core.apis.http.request.HttpRequest;
 import dan200.computercraft.core.apis.http.websocket.Websocket;
 import io.netty.handler.codec.http.DefaultHttpHeaders;
+import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpMethod;
+
+import javax.annotation.Nonnull;
+import java.net.URI;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+
+import static dan200.computercraft.core.apis.TableHelper.*;
 
 /**
  * The http library allows communicating with web servers, sending and receiving data from them.
@@ -41,147 +33,176 @@ import io.netty.handler.codec.http.HttpMethod;
  * @cc.module http
  * @hidden
  */
-public class HTTPAPI implements ILuaAPI {
-    private final IAPIEnvironment m_apiEnvironment;
+public class HTTPAPI implements ILuaAPI
+{
+    private final IAPIEnvironment apiEnvironment;
 
     private final ResourceGroup<CheckUrl> checkUrls = new ResourceGroup<>();
-    private final ResourceGroup<HttpRequest> requests = new ResourceQueue<>(() -> ComputerCraft.httpMaxRequests);
-    private final ResourceGroup<Websocket> websockets = new ResourceGroup<>(() -> ComputerCraft.httpMaxWebsockets);
+    private final ResourceGroup<HttpRequest> requests = new ResourceQueue<>( () -> ComputerCraft.httpMaxRequests );
+    private final ResourceGroup<Websocket> websockets = new ResourceGroup<>( () -> ComputerCraft.httpMaxWebsockets );
 
-    public HTTPAPI(IAPIEnvironment environment) {
-        this.m_apiEnvironment = environment;
+    public HTTPAPI( IAPIEnvironment environment )
+    {
+        apiEnvironment = environment;
     }
 
     @Override
-    public String[] getNames() {
-        return new String[] {"http"};
+    public String[] getNames()
+    {
+        return new String[] { "http" };
     }
 
     @Override
-    public void startup() {
-        this.checkUrls.startup();
-        this.requests.startup();
-        this.websockets.startup();
+    public void startup()
+    {
+        checkUrls.startup();
+        requests.startup();
+        websockets.startup();
     }
 
     @Override
-    public void update() {
+    public void shutdown()
+    {
+        checkUrls.shutdown();
+        requests.shutdown();
+        websockets.shutdown();
+    }
+
+    @Override
+    public void update()
+    {
         // It's rather ugly to run this here, but we need to clean up
         // resources as often as possible to reduce blocking.
         Resource.cleanup();
     }
 
-    @Override
-    public void shutdown() {
-        this.checkUrls.shutdown();
-        this.requests.shutdown();
-        this.websockets.shutdown();
-    }
-
     @LuaFunction
-    public final Object[] request(IArguments args) throws LuaException {
+    public final Object[] request( IArguments args ) throws LuaException
+    {
         String address, postString, requestMethod;
         Map<?, ?> headerTable;
         boolean binary, redirect;
 
-        if (args.get(0) instanceof Map) {
-            Map<?, ?> options = args.getTable(0);
-            address = getStringField(options, "url");
-            postString = optStringField(options, "body", null);
-            headerTable = optTableField(options, "headers", Collections.emptyMap());
-            binary = optBooleanField(options, "binary", false);
-            requestMethod = optStringField(options, "method", null);
-            redirect = optBooleanField(options, "redirect", true);
+        if( args.get( 0 ) instanceof Map )
+        {
+            Map<?, ?> options = args.getTable( 0 );
+            address = getStringField( options, "url" );
+            postString = optStringField( options, "body", null );
+            headerTable = optTableField( options, "headers", Collections.emptyMap() );
+            binary = optBooleanField( options, "binary", false );
+            requestMethod = optStringField( options, "method", null );
+            redirect = optBooleanField( options, "redirect", true );
 
-        } else {
+        }
+        else
+        {
             // Get URL and post information
-            address = args.getString(0);
-            postString = args.optString(1, null);
-            headerTable = args.optTable(2, Collections.emptyMap());
-            binary = args.optBoolean(3, false);
+            address = args.getString( 0 );
+            postString = args.optString( 1, null );
+            headerTable = args.optTable( 2, Collections.emptyMap() );
+            binary = args.optBoolean( 3, false );
             requestMethod = null;
             redirect = true;
         }
 
-        HttpHeaders headers = getHeaders(headerTable);
+        HttpHeaders headers = getHeaders( headerTable );
 
         HttpMethod httpMethod;
-        if (requestMethod == null) {
+        if( requestMethod == null )
+        {
             httpMethod = postString == null ? HttpMethod.GET : HttpMethod.POST;
-        } else {
-            httpMethod = HttpMethod.valueOf(requestMethod.toUpperCase(Locale.ROOT));
-            if (httpMethod == null || requestMethod.equalsIgnoreCase("CONNECT")) {
-                throw new LuaException("Unsupported HTTP method");
+        }
+        else
+        {
+            httpMethod = HttpMethod.valueOf( requestMethod.toUpperCase( Locale.ROOT ) );
+            if( httpMethod == null || requestMethod.equalsIgnoreCase( "CONNECT" ) )
+            {
+                throw new LuaException( "Unsupported HTTP method" );
             }
         }
 
-        try {
-            URI uri = HttpRequest.checkUri(address);
-            HttpRequest request = new HttpRequest(this.requests, this.m_apiEnvironment, address, postString, headers, binary, redirect);
+        try
+        {
+            URI uri = HttpRequest.checkUri( address );
+            HttpRequest request = new HttpRequest( requests, apiEnvironment, address, postString, headers, binary, redirect );
 
             // Make the request
-            request.queue(r -> r.request(uri, httpMethod));
+            request.queue( r -> r.request( uri, httpMethod ) );
 
-            return new Object[] {true};
-        } catch (HTTPRequestException e) {
-            return new Object[] {
-                false,
-                e.getMessage()
-            };
+            return new Object[] { true };
+        }
+        catch( HTTPRequestException e )
+        {
+            return new Object[] { false, e.getMessage() };
+        }
+    }
+
+    @LuaFunction
+    public final Object[] checkURL( String address )
+    {
+        try
+        {
+            URI uri = HttpRequest.checkUri( address );
+            new CheckUrl( checkUrls, apiEnvironment, address, uri ).queue( CheckUrl::run );
+
+            return new Object[] { true };
+        }
+        catch( HTTPRequestException e )
+        {
+            return new Object[] { false, e.getMessage() };
+        }
+    }
+
+    @LuaFunction
+    public final Object[] websocket( String address, Optional<Map<?, ?>> headerTbl ) throws LuaException
+    {
+        if( !ComputerCraft.http_websocket_enable )
+        {
+            throw new LuaException( "Websocket connections are disabled" );
+        }
+
+        HttpHeaders headers = getHeaders( headerTbl.orElse( Collections.emptyMap() ) );
+
+        try
+        {
+            URI uri = Websocket.checkUri( address );
+            if( !new Websocket( websockets, apiEnvironment, uri, address, headers ).queue( Websocket::connect ) )
+            {
+                throw new LuaException( "Too many websockets already open" );
+            }
+
+            return new Object[] { true };
+        }
+        catch( HTTPRequestException e )
+        {
+            return new Object[] { false, e.getMessage() };
         }
     }
 
     @Nonnull
-    private static HttpHeaders getHeaders(@Nonnull Map<?, ?> headerTable) throws LuaException {
+    private HttpHeaders getHeaders( @Nonnull Map<?, ?> headerTable ) throws LuaException
+    {
         HttpHeaders headers = new DefaultHttpHeaders();
-        for (Map.Entry<?, ?> entry : headerTable.entrySet()) {
+        for( Map.Entry<?, ?> entry : headerTable.entrySet() )
+        {
             Object value = entry.getValue();
-            if (entry.getKey() instanceof String && value instanceof String) {
-                try {
-                    headers.add((String) entry.getKey(), value);
-                } catch (IllegalArgumentException e) {
-                    throw new LuaException(e.getMessage());
+            if( entry.getKey() instanceof String && value instanceof String )
+            {
+                try
+                {
+                    headers.add( (String) entry.getKey(), value );
+                }
+                catch( IllegalArgumentException e )
+                {
+                    throw new LuaException( e.getMessage() );
                 }
             }
         }
+
+        if( !headers.contains( HttpHeaderNames.USER_AGENT ) )
+        {
+            headers.set( HttpHeaderNames.USER_AGENT, apiEnvironment.getComputerEnvironment().getUserAgent() );
+        }
         return headers;
-    }
-
-    @LuaFunction
-    public final Object[] checkURL(String address) {
-        try {
-            URI uri = HttpRequest.checkUri(address);
-            new CheckUrl(this.checkUrls, this.m_apiEnvironment, address, uri).queue(CheckUrl::run);
-
-            return new Object[] {true};
-        } catch (HTTPRequestException e) {
-            return new Object[] {
-                false,
-                e.getMessage()
-            };
-        }
-    }
-
-    @LuaFunction
-    public final Object[] websocket(String address, Optional<Map<?, ?>> headerTbl) throws LuaException {
-        if (!ComputerCraft.http_websocket_enable) {
-            throw new LuaException("Websocket connections are disabled");
-        }
-
-        HttpHeaders headers = getHeaders(headerTbl.orElse(Collections.emptyMap()));
-
-        try {
-            URI uri = Websocket.checkUri(address);
-            if (!new Websocket(this.websockets, this.m_apiEnvironment, uri, address, headers).queue(Websocket::connect)) {
-                throw new LuaException("Too many websockets already open");
-            }
-
-            return new Object[] {true};
-        } catch (HTTPRequestException e) {
-            return new Object[] {
-                false,
-                e.getMessage()
-            };
-        }
     }
 }
