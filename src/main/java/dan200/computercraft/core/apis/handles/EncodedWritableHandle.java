c0506108f93aaace01a01b394eab0c539eaf2b40
Jummit <jummit@web.de>
2021-05-15 23:26:06 +0200

    Fixed missing argument names in file handle docs (#632)

--- src/main/java/dan200/computercraft/core/apis/handles/EncodedWritableHandle.java
+++ src/main/java/dan200/computercraft/core/apis/handles/EncodedWritableHandle.java
@@ -54,7 +54,7 @@ public class EncodedWritableHandle extends HandleGeneric {
      *
      * @param args The value to write.
      * @throws LuaException If the file has been closed.
-     * @cc.param The value to write to the file.
+     * @cc.param value The value to write to the file.
      */
     @LuaFunction
     public final void write(IArguments args) throws LuaException {
@@ -72,7 +72,7 @@ public class EncodedWritableHandle extends HandleGeneric {
      *
      * @param args The value to write.
      * @throws LuaException If the file has been closed.
-     * @cc.param The value to write to the file.
+     * @cc.param value The value to write to the file.
      */
     @LuaFunction
     public final void writeLine(IArguments args) throws LuaException {
