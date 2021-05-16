coding-1ab/modpack-season-2

Fix disk recipes
Closes #652. This has been broken since the 1.13 update. Not filling
myself with confidence here.

mods/CC-Tweaked/src/main/java/dan200/computercraft/shared/util/ColourTracker.java
@@ -5,6 +5,7 @@
  */
 package dan200.computercraft.shared.util;
 
+import net.minecraft.item.DyeColor;
 import net.minecraft.item.crafting.ArmorDyeRecipe;
 
 /**
@@ -31,6 +32,12 @@ public class ColourTracker
     public void addColour( float r, float g, float b )
     {
         addColour( (int) (r * 255), (int) (g * 255), (int) (b * 255) );
+    }
+
+    public void addColour( DyeColor dye )
+    {
+        Colour colour = Colour.VALUES[15 - dye.getId()];
+        addColour( colour.getR(), colour.getG(), colour.getB() );
     }
 
     public boolean hasColour()

cc-tweaked/CC-Tweaked

src/main/java/dan200/computercraft/shared/util/ColourTracker.java
@@ -6,6 +6,8 @@
 
 package dan200.computercraft.shared.util;
 
+import net.minecraft.util.DyeColor;
+
 /**
  * A reimplementation of the colour system in {@link ArmorDyeRecipe}, but bundled together as an object.
  */
@@ -28,8 +30,15 @@ public class ColourTracker {
         this.count++;
     }
 
-    public boolean hasColour() {
-        return this.count > 0;
+    public void addColour( DyeColor dye )
+    {
+        Colour colour = Colour.VALUES[15 - dye.getId()];
+        addColour( colour.getR(), colour.getG(), colour.getB() );
+    }
+
+    public boolean hasColour()
+    {
+        return count > 0;
     }
 
     public int getColour() {