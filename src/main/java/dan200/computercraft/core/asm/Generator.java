coding-1ab/modpack-season-2

Prevent reflection errors crashing the game
.getMethods() may throw if a method references classes which don't exist
(such as client-only classes on a server). This is an Error, and so is
unchecked - hence us not handling it before.

Fixes #645

mods/CC-Tweaked/src/main/java/dan200/computercraft/core/asm/Generator.java
@@ -56,11 +56,11 @@ public final class Generator<T>
 
     private final LoadingCache<Class<?>, List<NamedMethod<T>>> classCache = CacheBuilder
         .newBuilder()
-        .build( CacheLoader.from( this::build ) );
+        .build( CacheLoader.from( catching( this::build, Collections.emptyList() ) ) );
 
     private final LoadingCache<Method, Optional<T>> methodCache = CacheBuilder
         .newBuilder()
-        .build( CacheLoader.from( this::build ) );
+        .build( CacheLoader.from( catching( this::build, Optional.empty() ) ) );
 
     Generator( Class<T> base, List<Class<?>> context, Function<T, T> wrap )
     {
@@ -357,5 +357,23 @@ public final class Generator<T>
         ComputerCraft.log.error( "Unknown parameter type {} for method {}.{}.",
             arg.getName(), method.getDeclaringClass().getName(), method.getName() );
         return null;
+    }
+
+    @SuppressWarnings( "Guava" )
+    private static <T, U> com.google.common.base.Function<T, U> catching( Function<T, U> function, U def )
+    {
+        return x -> {
+            try
+            {
+                return function.apply( x );
+            }
+            catch( Exception | LinkageError e )
+            {
+                // LinkageError due to possible codegen bugs and NoClassDefFoundError. The latter occurs when fetching
+                // methods on a class which references non-existent (i.e. client-only) types.
+                ComputerCraft.log.error( "Error generating @LuaFunctions", e );
+                return def;
+            }
+        };
     }
 }

cc-tweaked/CC-Tweaked

src/main/java/dan200/computercraft/core/asm/Generator.java
@@ -64,9 +64,9 @@ public final class Generator<T> {
 
     private final Function<T, T> wrap;
     private final LoadingCache<Method, Optional<T>> methodCache = CacheBuilder.newBuilder()
-                                                                              .build(CacheLoader.from(this::build));
+                                                                              .build(CacheLoader.from(catching(this::build, Optional.empty())));
     private final LoadingCache<Class<?>, List<NamedMethod<T>>> classCache = CacheBuilder.newBuilder()
-                                                                                        .build(CacheLoader.from(this::build));
+                                                                                        .build(CacheLoader.from(catching(this::build, Collections.emptyList())));
 
     Generator(Class<T> base, List<Class<?>> context, Function<T, T> wrap) {
         this.base = base;
@@ -373,5 +373,23 @@ public final class Generator<T> {
                                       .getName(),
                                 method.getName());
         return null;
+    }
+
+    @SuppressWarnings( "Guava" )
+    private static <T, U> com.google.common.base.Function<T, U> catching( Function<T, U> function, U def )
+    {
+        return x -> {
+            try
+            {
+                return function.apply( x );
+            }
+            catch( Exception | LinkageError e )
+            {
+                // LinkageError due to possible codegen bugs and NoClassDefFoundError. The latter occurs when fetching
+                // methods on a class which references non-existent (i.e. client-only) types.
+                ComputerCraft.log.error( "Error generating @LuaFunctions", e );
+                return def;
+            }
+        };
     }
 }