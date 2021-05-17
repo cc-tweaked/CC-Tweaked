67350616165bc8a1c6a324820f71f84dd71ced2a
Lupus590 <lupussolitarius590@gmail.com>
2021-05-17 11:53:23 +0100

    fix lint

--- src/main/resources/data/computercraft/lua/rom/modules/main/cc/expect.lua
+++ src/main/resources/data/computercraft/lua/rom/modules/main/cc/expect.lua
@@ -95,8 +95,8 @@ end
 --- Expect a number to be within a specific range.
 --
 -- @tparam number num, The value to check.
--- @tparam min The minimum value, if nil then `-math.huge` is used.
--- @tparam max The maximum value, if nil then `math.huge` is used.
+-- @tparam number min The minimum value, if nil then `-math.huge` is used.
+-- @tparam number max The maximum value, if nil then `math.huge` is used.
 -- @return The given `value`.
 -- @throws If the value is outside of the allowed range.
 local function range(num, min, max)
@@ -108,7 +108,7 @@ local function range(num, min, max)
   end
   
   if isNan(num) or num < min or num > max then
-      error(("number outside of range (expected %s to be within %s and %s)"):format(tostring(num), tostring(min), tostring(max)), 3)
+      error(("number outside of range (expected %d to be within %d and %d)"):format(num, min, max), 3)
   end
   
   return num
