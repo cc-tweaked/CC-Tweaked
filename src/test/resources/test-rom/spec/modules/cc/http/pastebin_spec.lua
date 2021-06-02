bd33926ca746e701a42da90166be600c7156d3b5
= <owenpatrickdrysdale@gmail.com>
2021-06-02 20:34:17 +0000

    Fix trailing whitespace

--- src/test/resources/test-rom/spec/modules/cc/http/pastebin_spec.lua
+++ src/test/resources/test-rom/spec/modules/cc/http/pastebin_spec.lua
@@ -1,13 +1,13 @@
 describe("cc.http.pastebin", function()
   local paste = require("cc.http.pastebin")
-  
+
   describe("get", function()
     it("gets a string from pastebin", function()
       local pasteString = paste.get("GW6SRgk8")
       expect(pasteString):eq("This is a test.")
     end)
   end)
-  
+
   describe("put", function()
     it("puts a paste onto pastebin", function()
       local pasteString = "test"
