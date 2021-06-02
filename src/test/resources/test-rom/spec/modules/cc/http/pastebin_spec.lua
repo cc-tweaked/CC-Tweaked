23ea4cea0a6d99a154e8c63c556a8c31edda1a34
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2021-06-02 14:30:18 -0400

    Create pastebin_spec.lua

---
+++ src/test/resources/test-rom/spec/modules/cc/http/pastebin_spec.lua
@@ -0,0 +1,18 @@
+describe("cc.http.pastebin",function()
+  local paste = require("cc.http.pastebin")
+  
+  describe("get",function()
+    it("gets a string from pastebin",function()
+      local pasteString = paste.get("9y5DpvAW")
+      expect(pasteString):eq("test")
+    end)
+  end)
+  
+  describe("put",function()
+    it("puts a paste onto pastebin", function()
+      local pasteString = "test"
+      local pasteID = paste.put(pasteString)
+      expect(type(pasteID)):eq("string")
+    end)
+  end)
+end)
