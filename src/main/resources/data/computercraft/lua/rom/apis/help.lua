d072cec0477a0481093c6c3cf4e352c1e31ecb52
Jonathan Coates <git@squiddev.cc>
2021-06-10 20:01:53 +0100

    Add very simple markdown support to the help viewer
     - Allow help files to use the ".md" suffix, and move changelog/whatsnew
       to use them.
    
     - When files end with ".md", the "help" program attempts to highlight
       them. This involves:
       - Colour code blocks with a lightGrey background.
       - Replace lists to use bullet points instead of "-"/"*".
       - Colours headings yellow.
       The implementation of this is a bit janky because a) I wrote this and
       b) we need to run this step before text wrapping, but preserve
       colours and section positions over wrapping (thanks to Jack for
       getting this working).
    
     - Add section navigation to the help viewer, with left/right to move to
       the next/previous section.
    
    Closes #569

--- src/main/resources/data/computercraft/lua/rom/apis/help.lua
+++ src/main/resources/data/computercraft/lua/rom/apis/help.lua
@@ -27,21 +27,24 @@ function setPath(_sPath)
     sPath = _sPath
 end
 
+local extensions = { "", ".md", ".txt" }
+
 --- Returns the location of the help file for the given topic.
 --
 -- @tparam string topic The topic to find
 -- @treturn string|nil The path to the given topic's help file, or `nil` if it
 -- cannot be found.
 -- @usage help.lookup("disk")
-function lookup(_sTopic)
-    expect(1, _sTopic, "string")
+function lookup(topic)
+    expect(1, topic, "string")
     -- Look on the path variable
-    for sPath in string.gmatch(sPath, "[^:]+") do
-        sPath = fs.combine(sPath, _sTopic)
-        if fs.exists(sPath) and not fs.isDir(sPath) then
-            return sPath
-        elseif fs.exists(sPath .. ".txt") and not fs.isDir(sPath .. ".txt") then
-            return sPath .. ".txt"
+    for path in string.gmatch(sPath, "[^:]+") do
+        path = fs.combine(path, topic)
+        for _, extension in ipairs(extensions) do
+            local file = path .. extension
+            if fs.exists(file) and not fs.isDir(file) then
+                return file
+            end
         end
     end
 
@@ -66,8 +69,11 @@ function topics()
             for _, sFile in pairs(tList) do
                 if string.sub(sFile, 1, 1) ~= "." then
                     if not fs.isDir(fs.combine(sPath, sFile)) then
-                        if #sFile > 4 and sFile:sub(-4) == ".txt" then
-                            sFile = sFile:sub(1, -5)
+                        for i = 2, #extensions do
+                            local extension = extensions[i]
+                            if #sFile > #extension and sFile:sub(-#extension) == extension then
+                                sFile = sFile:sub(1, -#extension - 1)
+                            end
                         end
                         tItems[sFile] = true
                     end
