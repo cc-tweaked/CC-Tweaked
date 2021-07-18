0ab261370cc905bae48641c16f07d3000b49ef47
JackMacWindows <jackmacwindowslinux@gmail.com>
2021-07-18 14:37:06 -0400

    `parallel` no longer errors on no arguments

--- src/main/resources/data/computercraft/lua/rom/apis/parallel.lua
+++ src/main/resources/data/computercraft/lua/rom/apis/parallel.lua
@@ -18,7 +18,6 @@ the other.
 local function create(...)
     local tFns = table.pack(...)
     local tCos = {}
-    if tFns.n < 1 then error("bad argument #1 (expected function, got nil)", 3) end
     for i = 1, tFns.n, 1 do
         local fn = tFns[i]
         if type(fn) ~= "function" then
@@ -33,6 +32,7 @@ end
 
 local function runUntilLimit(_routines, _limit)
     local count = #_routines
+    if count < 1 then return 0 end
     local living = count
 
     local tFilters = {}
