0568c8662837f44b0213505215c50294dd563812
Jonathan Coates <git@squiddev.cc>
2021-07-24 00:00:37 +0100

    Hopefully fix flakiness in rednet test

--- src/test/server-files/computers/computer/13/startup.lua
+++ src/test/server-files/computers/computer/13/startup.lua
@@ -2,11 +2,11 @@
 
 rednet.open("top")
 
-rednet.send(14, "Test msg")
-
 local id, msg
 repeat
-    id, msg = rednet.receive()
+    rednet.send(14, "Test msg") -- Keep sending, as other computer may not have started yet.
+
+    id, msg = rednet.receive(nil, 1)
     print(id, msg)
 until id == 14
 
