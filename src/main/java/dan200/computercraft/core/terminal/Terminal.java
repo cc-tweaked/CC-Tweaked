06bf84f1512c68eefcdedb16edb44e9dcb84de4c
i develop things <50844998+Ocawesome101@users.noreply.github.com>
2021-10-03 03:11:31 -0700

    Make color arguments to `term.blit` case-insensitive (#929)

--- src/main/java/dan200/computercraft/core/terminal/Terminal.java
+++ src/main/java/dan200/computercraft/core/terminal/Terminal.java
@@ -419,6 +419,7 @@ public class Terminal
     {
         if( c >= '0' && c <= '9' ) return c - '0';
         if( c >= 'a' && c <= 'f' ) return c - 'a' + 10;
+        if( c >= 'A' && c <= 'F' ) return c - 'A' + 10;
         return 15 - def.ordinal();
     }
 }
