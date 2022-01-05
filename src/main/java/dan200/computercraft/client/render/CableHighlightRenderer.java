1102ad6452d7363fce5ffd6e27c9c016a1bd57c8
Toad-Dev <748280+toad-dev@users.noreply.github.com>
2022-01-04 20:43:45 -0800

    Update license headers for 2022.

--- src/main/java/dan200/computercraft/client/render/CableHighlightRenderer.java
+++ src/main/java/dan200/computercraft/client/render/CableHighlightRenderer.java
@@ -1,9 +1,8 @@
 /*
  * This file is part of ComputerCraft - http://www.computercraft.info
- * Copyright Daniel Ratcliffe, 2011-2021. Do not distribute without permission.
+ * Copyright Daniel Ratcliffe, 2011-2022. Do not distribute without permission.
  * Send enquiries to dratcliffe@gmail.com
  */
-
 package dan200.computercraft.client.render;
 
 import dan200.computercraft.shared.ComputerCraftRegistry;
