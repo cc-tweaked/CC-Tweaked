772e7f9f873d6c0e6f802a8009bce08701f88866
i develop things <50844998+Ocawesome101@users.noreply.github.com>
2022-01-20 18:27:00 -0500

    add concatenation utility

---
+++ src/main/resources/data/computercraft/lua/rom/programs/concat.lua
@@ -0,0 +1,30 @@
+local tArgs = { ... }
+
+if #tArgs < 2 then
+  printError("Usage: concat <file> [...] <destination>")
+end
+
+-- The data to write
+local sToWrite = ""
+-- The file to which to write it
+local sDestination = tArgs[#tArgs]
+tArgs[#tArgs] = nil
+
+for _, sItem in ipairs(tArgs) do
+  local handle, sError = fs.open(sItem, "rb")
+  if not handle then
+    printError(sItem .. ": " .. sError)
+    return
+  end
+  sToWrite = sToWrite .. (handle.readAll() or "")
+  handle.close()
+end
+
+local outHandle, sError = fs.open(sDestination, "wb")
+if not outHandle then
+  printError(sDestination .. ": " .. sError)
+  return
+end
+
+outHandle.write(sToWrite)
+outHandle.close()
