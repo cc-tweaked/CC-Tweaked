969feb4a1c9d17d39aa29c610f828ca78ce088c2
Luiz Krüger <46863600+itisluiz@users.noreply.github.com>
2022-07-16 17:30:20 -0300

    ItemGroup info on getItemDetail (#1127)

--- src/main/java/dan200/computercraft/shared/peripheral/generic/methods/InventoryMethods.java
+++ src/main/java/dan200/computercraft/shared/peripheral/generic/methods/InventoryMethods.java
@@ -107,7 +107,9 @@ public class InventoryMethods implements GenericPeripheral
      *
      * The returned information contains the same information as each item in
      * {@link #list}, as well as additional details like the display name
-     * (`displayName`) and item durability (`damage`, `maxDamage`, `durability`).
+     * (`displayName`), item groups (`itemGroups`), which are the creative tabs
+     * an item will appear under, and item and item durability (`damage`,
+     * `maxDamage`, `durability`).
      *
      * Some items include more information (such as enchantments) - it is
      * recommended to print it out using @{textutils.serialize} or in the Lua
@@ -127,6 +129,11 @@ public class InventoryMethods implements GenericPeripheral
      *
      * print(("%s (%s)"):format(item.displayName, item.name))
      * print(("Count: %d/%d"):format(item.count, item.maxCount))
+     *
+     * for _, group in pairs(item.itemGroups) do
+     *   print(("Group: %s"):format(group.displayName))
+     * end
+     *
      * if item.damage then
      *   print(("Damage: %d/%d"):format(item.damage, item.maxDamage))
      * end
