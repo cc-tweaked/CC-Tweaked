4c5b3a6ee592b89f5fa98f709ba543501b70f5d5
Jonathan Coates <git@squiddev.cc>
2022-10-31 17:46:02 +0000

    Clear Origin header on websockets
    Technically this removes Sec-Websocket-Origin, as that's what the
    current version of Netty uses. We'll need to change this on 1.18+.
    
    Closes ##1197.

---
+++ src/main/java/dan200/computercraft/core/apis/http/websocket/NoOriginWebSocketHanshakder.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of ComputerCraft - http://www.computercraft.info
+ * Copyright Daniel Ratcliffe, 2011-2022. Do not distribute without permission.
+ * Send enquiries to dratcliffe@gmail.com
+ */
+package dan200.computercraft.core.apis.http.websocket;
+
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshaker13;
+import io.netty.handler.codec.http.websocketx.WebSocketVersion;
+
+import java.net.URI;
+
+/**
+ * A version of {@link WebSocketClientHandshaker13} which doesn't add the {@link HttpHeaderNames#SEC_WEBSOCKET_ORIGIN}
+ * header to the original HTTP request.
+ */
+public class NoOriginWebSocketHanshakder extends WebSocketClientHandshaker13
+{
+    public NoOriginWebSocketHanshakder( URI webSocketURL, WebSocketVersion version, String subprotocol, boolean allowExtensions, HttpHeaders customHeaders, int maxFramePayloadLength )
+    {
+        super( webSocketURL, version, subprotocol, allowExtensions, customHeaders, maxFramePayloadLength );
+    }
+
+    @Override
+    protected FullHttpRequest newHandshakeRequest()
+    {
+        FullHttpRequest request = super.newHandshakeRequest();
+        HttpHeaders headers = request.headers();
+
+        if( !customHeaders.contains( HttpHeaderNames.SEC_WEBSOCKET_ORIGIN ) )
+        {
+            headers.remove( HttpHeaderNames.SEC_WEBSOCKET_ORIGIN );
+        }
+        return request;
+    }
+}
