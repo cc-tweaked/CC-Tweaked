1259e29f213376bca7f11fd9644b71696a140676
Jonathan Coates <git@squiddev.cc>
2022-12-29 22:30:09 +0000

    Fix full-block wired modems not connecting
    Closes #1278.

--- projects/common/src/main/java/dan200/computercraft/shared/peripheral/modem/wired/WiredModemFullBlockEntity.java
+++ projects/common/src/main/java/dan200/computercraft/shared/peripheral/modem/wired/WiredModemFullBlockEntity.java
@@ -219,7 +219,7 @@ public class WiredModemFullBlockEntity extends BlockEntity {
             if (!world.isLoaded(offset)) continue;
 
             var element = connectedElements.get((ServerLevel) getLevel(), getBlockPos(), facing);
-            if (element == null) return;
+            if (element == null) continue;
 
             node.connectTo(element.getNode());
         }
