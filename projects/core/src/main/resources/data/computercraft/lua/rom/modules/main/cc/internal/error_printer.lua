e75cc1cb49bacf73f4a2b04c134e419b89d990f9
Jonathan Coates <git@squiddev.cc>
2023-01-23 20:49:19 +0000

    Rewrite the error printer
    I wrote this on the train home from work, so this is not good code. I
    don't even know if illuaminate is happy with it.

--- projects/core/src/main/resources/data/computercraft/lua/rom/modules/main/cc/internal/error_printer.lua
+++ projects/core/src/main/resources/data/computercraft/lua/rom/modules/main/cc/internal/error_printer.lua
@@ -16,55 +16,78 @@ local expect = require "cc.expect"
 local wrap = require "cc.strings".wrap
 local expect, field = expect.expect, expect.field
 
+--- Write a message to the screen.
+-- @tparam cc.pretty.Doc|string msg The message to write.
 local function display(msg)
     if type(msg) == "table" then pretty.print(msg) else print(msg) end
 end
 
-local function get_len(msg)
-    if type(msg) == "string" then return #msg end
-
-    local kind = msg.tag
-    if kind == "nil" or kind == "line" then
-        return 0
-    elseif kind == "text" then
-        return #msg.text
-    elseif kind == "concat" then
-        local len = 0
-        for i = 1, msg.n do
-            len = len + get_len(msg[i])
-        end
-        return len
-    else
-        error("Unknown doc " .. kind)
-    end
-end
-
-local function display_here(msg)
+-- Write a message to the screen, aligning to the current cursor position.
+-- @tparam cc.pretty.Doc|string msg The message to write.
+local function display_here(msg, preamble)
+    expect(1, msg, "string", "table")
     local x = term.getCursorPos()
     local width, height = term.getSize()
     width = width - x + 1
+
+    local function newline()
+        local _, y = term.getCursorPos()
+        if y >= height then
+            term.scroll(1)
+        else
+            y = y + 1
+        end
+
+        preamble(y)
+        term.setCursorPos(x, y)
+    end
 
     if type(msg) == "string" then
         local lines = wrap(msg, width)
         term.write(lines[1])
         for i = 2, #lines do
-            local _, y = term.getCursorPos()
-            if y >= height then
-                term.scroll(1)
-            else
-                y = y + 1
-            end
-            term.setCursorPos(1, y)
-            term.blit("\x95", "9", "f")
-            term.setCursorPos(x, y)
-
+            newline()
             term.write(lines[i])
         end
         print()
     else
-        pretty.print(msg)
+        local def_colour = term.getTextColour()
+        local function display_impl(doc)
+            expect(1, doc, "table")
+            local kind = doc.tag
+            if kind == "nil" then return
+            elseif kind == "text" then
+                -- TODO: cc.strings.wrap doesn't support a leading indent. We should
+                -- fix that!
+                -- Might also be nice to add a wrap_iter, which returns an iterator over
+                -- start_pos, end_pos instead.
+
+                if doc.colour then term.setTextColour(doc.colour) end
+                local x1 = term.getCursorPos()
+
+                local lines = wrap((" "):rep(x1 - x) .. doc.text, width)
+                term.write(lines[1]:sub(x1 - x + 1))
+                for i = 2, #lines do
+                    newline()
+                    term.write(lines[i])
+                end
+
+                if doc.colour then term.setTextColour(def_colour) end
+            elseif kind == "concat" then
+                for i = 1, doc.n do display_impl(doc[i]) end
+            else
+                error("Unknown doc " .. kind)
+            end
+        end
+        display_impl(msg)
     end
 end
+
+--- A list of colours we can use for error messages.
+local error_colours = { colours.red, colours.green, colours.magenta, colours.orange }
+
+--- The accent line used to denote a block of code.
+local code_accent = pretty.text("\x95", colours.cyan)
 
 --[[-
 @tparam string input The input string.
@@ -77,94 +100,141 @@ return function(input, get_pos, message)
     expect(3, message, "table")
     if #message == 0 then error("Message is empty", 2) end
 
-    local start = pretty.text("\x95", colours.cyan)
-    local error_colours, error_colour = { colours.red, colours.green, colours.magenta, colours.orange }, 1
+    local error_colour = 1
+    local width = term.getSize()
 
-    local drawn = false
+    local msg_idx, msg_n = 1, #message
+    while msg_idx <= msg_n do
+        if msg_idx > 1 then print() end
 
-    local function make_message(msg)
-        expect(1, msg, "table")
-        field(msg, "msg", "string", "table")
-        field(msg, "col", "number")
-        field(msg, "line", "number")
-        field(msg, "end_col", "number")
-        field(msg, "end_line", "number")
-
-        local indicator, indicator_col
-        if msg.line ~= msg.end_line then
-            indicator = "\x83" -- TODO: Is this the best way?
-            indicator_col = msg.col + 1
-        else
-            indicator = ("\x83"):rep(msg.end_col - msg.col + 1)
-            indicator_col = math.floor((msg.end_col + msg.col) / 2)
-        end
-
-        local offset = indicator_col - msg.col + 1
-        indicator = indicator:sub(1, offset - 1) ..
-            string.char(bit32.bor(0x14, indicator:byte(offset))) ..
-            indicator:sub(offset + 1)
-
-        msg.indicator = indicator
-        msg.indicator_col = indicator_col
-        msg.colour = error_colours[error_colour]
-        error_colour = (error_colour % #error_colours) + 1
-
-        return msg
-    end
-
-    local current_line, current_bol, current_messages
-    local function flush_messages()
-        if not current_line then return end
-        if drawn then print() else drawn = true end
-
-        local contents = input:match("[^\r\n]*", current_bol)
-
-        pretty.print(start .. pretty.text("Line " .. current_line, colours.cyan))
-        pretty.print(start .. pretty.text(contents, colours.lightGrey))
-
-        local current_col = 1
-        pretty.write(start)
-        for _, msg in ipairs(current_messages) do
-            write((" "):rep(msg.col - current_col))
-            pretty.write(pretty.text(msg.indicator, msg.colour))
-            current_col = msg.end_col + 1
-        end
-        print()
-
-        local max_col = current_messages[#current_messages].indicator_col
-        for _, msg in ipairs(current_messages) do
-            pretty.write(
-                start ..
-                (" "):rep(msg.indicator_col - 1) ..
-                pretty.text("\x8d" .. ("\x8c"):rep(max_col - msg.indicator_col) .. " ", msg.colour)
-            )
-            display_here(msg.msg)
-        end
-
-        current_line, current_bol, current_messages = nil, nil, nil
-    end
-
-    for _, msg in ipairs(message) do
+        local msg = message[msg_idx]
         if type(msg) == "table" and msg.tag == "annotate" then
-            local line, col, bol = get_pos(msg.start_pos)
-            local end_line, end_col = get_pos(msg.end_pos)
+            local current_line, first_col, current_bol = get_pos(msg.start_pos)
+            local contents = input:match("[^\r\n]*", current_bol)
 
-            local full_msg = make_message {
-                msg = msg.msg, line = line, col = col, end_line = end_line, end_col = end_col,
-            }
+            -- Find the highest minimum column we could possibly start from, and thus
+            -- the last column we could bundle into this line.
+            local min_col = math.max(contents:find("%S") or 1, first_col - 5)
+            -- TODO: If the screen is less than 35(?) characters, swap over to numbered
+            -- messages.
+            local max_col = min_col + (width - 25)
 
-            if current_line == line then
-                current_messages[#current_messages + 1] = full_msg
-            else
-                flush_messages()
-                current_line, current_bol, current_messages = line, bol, { full_msg }
+            -- Gather all annotations that we can fit on this line.
+            local annotations = {}
+            while msg_idx <= msg_n do
+                local msg = message[msg_idx]
+                if type(msg) ~= "table" or msg.tag ~= "annotate" then break end
+
+                local line, col = get_pos(msg.start_pos)
+                if line ~= current_line or col > max_col then break end
+
+                local end_line, end_col = get_pos(msg.end_pos)
+                annotations[#annotations + 1] = {
+                    msg = msg.msg, colour = colours.toBlit(error_colours[error_colour]),
+                    col = col, end_line = end_line, end_col = end_col,
+                }
+                error_colour = (error_colour % #error_colours) + 1
+
+                msg_idx = msg_idx + 1
+            end
+
+            -- Now find the appropriate region of our line to display.
+            local last_col = annotations[#annotations].col
+            -- TODO: Would be nice to be smarter with how we pick our starting position.
+            local start_col = math.max(1, last_col - (max_col - min_col))
+
+            -- We do some additional work to display indicators if the line is truncated.
+            local str_start, str_end = start_col, start_col + width - 2
+            local prefix, suffix = "", ""
+            if start_col > 1 then
+                str_end = str_end - 1
+                start_col = start_col - 1
+                prefix = pretty.text("\xab", colours.grey)
+            end
+            if str_end < #contents then
+                str_end = str_end - 1
+                suffix = pretty.text("\xbb", colours.grey)
+            end
+
+            -- Print the line number and snippet of code.
+            pretty.print(code_accent .. pretty.text("Line " .. current_line, colours.cyan))
+            pretty.print(code_accent .. prefix .. pretty.text(contents:sub(str_start, str_end), colours.lightGrey) .. suffix)
+
+            -- Print the annotations' underlines.
+            local _, y = term.getCursorPos()
+            pretty.write(code_accent)
+            for i, annotation in ipairs(annotations) do
+                local indicator_end = annotation.end_col
+                if current_line ~= annotation.end_line or annotation.end_col > str_end then
+                    indicator_end = str_end
+                end
+
+                local indicator_len = indicator_end - annotation.col + 1
+                local indicator_start = annotation.col - start_col + 2
+
+                local fg, bg = annotation.colour, "f"
+                local indicator, indicator_fg, indicator_bg
+                if annotation.msg ~= "" then
+                    -- If we've got a message, find where to place our downward
+                    -- connector. Rendering the string is simple, just a little nasty
+                    -- due to the connector being inverted.
+                    local indicator_offset
+                    if indicator_end == str_end then
+                        indicator_offset = 0
+                    else
+                        indicator_offset = math.ceil(indicator_len / 2) - 1
+                    end
+                    annotation.indicator_col = indicator_start + indicator_offset
+
+                    indicator = ("\x83"):rep(indicator_offset) .. "\x94" .. ("\x83"):rep(indicator_len - indicator_offset - 1)
+                    indicator_bg = bg:rep(indicator_offset) .. fg .. bg:rep(indicator_len - indicator_offset - 1)
+                    indicator_fg = fg:rep(indicator_offset) .. bg .. fg:rep(indicator_len - indicator_offset - 1)
+                else
+                    -- If we've no message, we don't need a connector, so can just draw
+                    -- a straight line.
+                    indicator, indicator_fg, indicator_bg = ("\x83"):rep(indicator_len), fg:rep(indicator_len), bg:rep(indicator_len)
+                end
+
+                term.setCursorPos(indicator_start, y)
+                term.blit(indicator, indicator_fg, indicator_bg)
+            end
+            print()
+
+            -- Print the message for each annotation.
+            local last_col = annotations[#annotations].indicator_col
+            for i, annotation in ipairs(annotations) do
+                if annotation.msg ~= "" then
+                    local _, y = term.getCursorPos()
+                    pretty.write(code_accent)
+
+                    -- Start off by drawing our connector all the way to the right.
+                    local indicator_col = annotation.indicator_col
+                    local len = last_col - indicator_col + 2
+                    local indicator = "\x8a" .. ("\x8c"):rep(len - 2) .. ("\x84")
+                    local indicator_bg = ("f"):rep(len)
+                    local indicator_fg = annotation.colour:rep(len)
+                    term.setCursorPos(indicator_col, y)
+                    term.blit(indicator, indicator_fg, indicator_bg)
+
+                    -- Then print the message. For each additional line of our message,
+                    -- we need to display the code accent and any remaining connectors.
+                    display_here(annotation.msg, function(y)
+                        term.setCursorPos(1, y)
+                        pretty.write(code_accent)
+
+                        for j = i + 1, #annotations do
+                            local annotation = annotations[j]
+                            if annotation.msg ~= "" then
+                                term.setCursorPos(annotation.indicator_col, y)
+                                term.blit("\x95", "f", annotation.colour)
+                            end
+                        end
+                    end)
+                end
             end
         else
-            flush_messages()
-            if drawn then print() else drawn = true end
             display(msg)
+            msg_idx = msg_idx + 1
         end
     end
-
-    flush_messages()
 end
