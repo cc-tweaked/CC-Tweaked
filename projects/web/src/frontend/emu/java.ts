6702ab8f9d4c08e9bf2ef2bee704ac0c482859fb
Jonathan Coates <git@squiddev.cc>
2025-12-15 14:17:03 +0000

    Enable TeaVM WASM GC Backend
    I'd tried this ages ago, and had some issues I utterly failed to find a
    minimal reproducer for. Now it Just Works.

--- projects/web/src/frontend/emu/java.ts
+++ projects/web/src/frontend/emu/java.ts
@@ -4,11 +4,31 @@
 
 import "setimmediate";
 
-import type { ComputerDisplay, ComputerHandle } from "cct/classes";
-export type { ComputerDisplay, ComputerHandle, PeripheralKind, Side } from "cct/classes";
+import type { ComputerDisplay, ComputerHandle } from "cct/classes.js";
+export type { ComputerDisplay, ComputerHandle, PeripheralKind, Side } from "cct/classes.js";
+import { load as teaVMLoad } from "cct/wasm-gc-runtime.js";
+import { exceptions, gc } from "wasm-feature-detect";
+import wasmClasses from "cct/classes.wasm";
+
+const loadClasses = async (): Promise<{ main: (args: string[]) => void }> => {
+    if (
+        typeof WebAssembly === "object" && typeof WebAssembly.compileStreaming === "function" &&
+        await exceptions() && await gc()
+    ) {
+        try {
+            console.log("Loading WASM runtime");
+            return (await teaVMLoad(wasmClasses)).exports;
+        } catch (e) {
+            console.error("Failed to load WebAssembly runtime", e);
+        }
+    }
+
+    console.log("Using JS runtime");
+    return await import("cct/classes.js");
+}
 
 const load = async (): Promise<(computer: ComputerDisplay) => ComputerHandle> => {
-    const [classes, { version, resources }] = await Promise.all([import("cct/classes"), import("cct/resources")]);
+    const [classes, { version, resources }] = await Promise.all([loadClasses(), import("cct/resources.js")]);
 
     let addComputer: ((computer: ComputerDisplay) => ComputerHandle) | null = null;
     const encoder = new TextEncoder();
@@ -18,7 +38,7 @@ const load = async (): Promise<(computer: ComputerDisplay) => ComputerHandle> =>
         listResources: () => Object.keys(resources),
         getResource: path => new Int8Array(encoder.encode(resources[path]))
     };
-    classes.main();
+    classes.main([]);
 
     if (!addComputer) throw new Error("Callbacks.setup was never called");
     return addComputer;
