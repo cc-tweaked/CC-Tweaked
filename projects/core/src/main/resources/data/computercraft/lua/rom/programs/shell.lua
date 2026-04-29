abe3d3410fd446a2484383c8078bd9ef20b03aa4
Jonathan Coates <git@squiddev.cc>
2026-04-29 11:42:56 +0100

    Remove pathWithExtension
    This doesn't actually do anything: paths will never have a trailing
    slash, so it's just equivalent to `x..".lua"`.

--- projects/core/src/main/resources/data/computercraft/lua/rom/programs/shell.lua
+++ projects/core/src/main/resources/data/computercraft/lua/rom/programs/shell.lua
@@ -350,16 +350,6 @@ function shell.resolve(path)
     end
 end
 
-local function pathWithExtension(_sPath, _sExt)
-    local nLen = #sPath
-    local sEndChar = string.sub(_sPath, nLen, nLen)
-    -- Remove any trailing slashes so we can add an extension to the path safely
-    if sEndChar == "/" or sEndChar == "\\" then
-        _sPath = string.sub(_sPath, 1, nLen - 1)
-    end
-    return _sPath .. "." .. _sExt
-end
-
 --- Resolve a program, using the [program path][`path`] and list of [aliases][`aliases`].
 --
 -- @tparam string command The name of the program
@@ -384,7 +374,7 @@ function shell.resolveProgram(command)
         if fs.exists(sPath) and not fs.isDir(sPath) then
             return sPath
         else
-            local sPathLua = pathWithExtension(sPath, "lua")
+            local sPathLua = sPath .. ".lua"
             if fs.exists(sPathLua) and not fs.isDir(sPathLua) then
                 return sPathLua
             end
@@ -398,7 +388,7 @@ function shell.resolveProgram(command)
         if fs.exists(sPath) and not fs.isDir(sPath) then
             return sPath
         else
-            local sPathLua = pathWithExtension(sPath, "lua")
+            local sPathLua = sPath .. ".lua"
             if fs.exists(sPathLua) and not fs.isDir(sPathLua) then
                 return sPathLua
             end
