ff25b2ec929cb8c300e1cc8a9c69e2ae2a254567
Jonathan Coates <git@squiddev.cc>
2026-05-26 14:42:37 +0100

    Kinda rewrite things
    The two parallel implementations are now entirely separate. I don't
    think we gained much from trying to share them.
    
     - Remove spawning support from parallel.waitForAny
     - Prevent spawning from outside the parallel block
     - Clean up spawned tasks
     - Fix spawned functions being called with an event. They can now be
       called with arbitrary arguments.
     - Docs

--- projects/core/src/main/resources/data/computercraft/lua/rom/apis/parallel.lua
+++ projects/core/src/main/resources/data/computercraft/lua/rom/apis/parallel.lua
@@ -39,75 +39,15 @@ the other.
 @since 1.2
 ]]
 
-local exception = dofile("rom/modules/main/cc/internal/tiny_require.lua")("cc.internal.exception")
-
-local function create(...)
-    local barrier_ctx = { co = coroutine.running() }
-
-    local threads = {}
-    -- TODO: After-the-fact summoning does not let the user modify the order in
-    -- which later tasks are executed in. This may be a consideration for later,
-    -- although I don't think it's a big deal. This should not replace libraries
-    -- for parallel execution, but instead provide tools for low-requirement
-    -- scripts to do some after-the-fact parallelism without third party code.
-    local function summon(...)
-        local functions = table.pack(...)
-        for i = 1, functions.n, 1 do
-            local fn = functions[i]
-            if type(fn) ~= "function" then
-                error("bad argument #" .. i .. " (function expected, got " .. type(fn) .. ")", 4)
-            end
-            table.insert(threads, {
-                co = coroutine.create(function() return exception.try_barrier(barrier_ctx, fn, summon) end),
-                filter = nil
-            })
-        end
-    end
-    summon(...)
-
-    return threads
-end
-
-local function run(threads, mustAllDead)
-    if #threads < 1 then return 0 end
-    local dead = 0
-
-    local event = { n = 0 }
-    while true do
-        local i = 1
-        while i <= #threads do
-            local thread = threads[i]
-            if thread and (thread.filter == nil or thread.filter == event[1] or event[1] == "terminate") then
-                local ok, param = coroutine.resume(thread.co, table.unpack(event, 1, event.n))
-                if ok then
-                    thread.filter = param
-                elseif type(param) == "string" and exception.can_wrap_errors() then
-                    error(exception.make_exception(param, thread.co))
-                else
-                    error(param, 0)
-                end
-
-                if coroutine.status(thread.co) == "dead" then
-                    threads[i] = false
-                    dead = dead + 1
-                    if (mustAllDead and dead == #threads) or
-                        (not mustAllDead and dead > 0) then
-                        return i
-                    end
-                end
-            end
-            i = i + 1
-        end
-
-        event = table.pack(os.pullEventRaw())
-    end
-end
+local require = dofile("rom/modules/main/cc/internal/tiny_require.lua")
+local expect = require("cc.expect").expect
+local exception = require("cc.internal.exception")
 
 --[[- Switches between execution of the functions, until any of them
 finishes. If any of the functions errors, the message is propagated upwards
 from the [`parallel.waitForAny`] call.
 
-@tparam function(summon: function|nil) ... The functions this task will run
+@tparam function ... The functions to run in parallel.
 @usage Print a message every second until the `q` key is pressed.
 
     local function tick()
@@ -127,15 +67,77 @@ from the [`parallel.waitForAny`] call.
     print("Everything done!")
 ]]
 function waitForAny(...)
-    local threads = create(...)
-    return run(threads, false)
+    local barrier_ctx = { co = coroutine.running() }
+
+    local functions = table.pack(...)
+    local threads = {}
+    for i = 1, functions.n do
+        local fn = functions[i]
+        expect(i, fn, "function")
+        threads[i] = {
+            co = coroutine.create(function() return exception.try_barrier(barrier_ctx, fn) end),
+            filter = nil,
+        }
+    end
+
+    local count = functions.n
+    if count < 1 then return 0 end
+
+    local event = { n = 0 }
+    while true do
+        for i = 1, count do
+            local thread = threads[i]
+            if thread.filter == nil or thread.filter == event[1] or event[1] == "terminate" then
+                local ok, param = coroutine.resume(thread.co, table.unpack(event, 1, event.n))
+                if not ok then
+                    error(exception.wrap_error(param, thread.co))
+                end
+
+                -- Abort if this coroutine has finished
+                if coroutine.status(thread.co) == "dead" then return i end
+
+                thread.filter = param
+            end
+        end
+
+        event = table.pack(os.pullEventRaw())
+    end
 end
 
---[[- Switches between execution of the functions, until all of them are
-finished. If any of the functions errors, the message is propagated upwards
-from the [`parallel.waitForAll`] call.
+--[[-
+Runs several functions in parallel, until all of them are finished.
 
-@tparam function(summon: function|nil) ... The functions this task will run
+If any of the functions errors, the other functions are not resumed, and the
+error is propagated upwards.
+
+> [!WARNING]
+>
+> While any number of coroutines can be run in parallel, running too many things
+> in parallel can sometimes cause issues:
+>
+>  - Computers only buffer 256 events at a time. Trying to run several hundred
+>    functions in parallel (particularly when calling peripheral methods) can
+>    cause the event queue to fill up, resulting in events being dropped, and
+>    programs getting stuck.
+>  - Computers only run 16 HTTP requests at a time. Trying to run more than that
+>    in parallel will have no effect.
+
+### Spawning new parallel functions
+In some cases, you may want to start running additional functions in parallel
+from an existing [`parallel.waitForAll`] call. Every function passed to
+[`waitForAll`] can accept a `spawn` argument, which can be called to spawn new
+parallel functions.
+
+```lua
+parallel.waitForAll(function(spawn)
+    spawn(function() sleep(1); print("Finished 1") end)
+    spawn(function() sleep(2); print("Finished 2") end)
+end)
+```
+
+@tparam function(spawn: function(fn: function, any...)) ... The functions to run
+in parallel.
+
 @usage Start off two timers and wait for them both to run.
 
     local function a()
@@ -150,24 +152,101 @@ from the [`parallel.waitForAll`] call.
     parallel.waitForAll(a, b)
     print("Everything done!")
 
-@usage Start new tasks during execution
+@usage Generate a list of functions to run in parallel.
 
-    local function b()
-        os.sleep(3)
-        print("B ran!")
-    end
-    local function a(summon)
-        os.sleep(1)
-        for _ = 1, 3 do
-            summon(b)
-        end
-        print("A is done")
+    local funcs = {}
+    for i = 1, 5 do
+        table.insert(funcs, function()
+            sleep(math.random())
+            print("Finished " .. i)
+        end)
     end
 
-    parallel.waitForAll(a)
+    parallel.waitForAll(table.unpack(funcs))
     print("Everything done!")
+
+@usage Run new functions in parallel from within `waitForAll`.
+
+    parallel.waitForAll(function(spawn)
+        for i = 1, 5 do
+            spawn(function()
+                sleep(math.random())
+                print("Finished " .. i)
+            end)
+        end
+    end)
+    print("Everything done!")
+
+@changed 1.120.0 Added ability to spawn new parallel functions.
 ]]
 function waitForAll(...)
-    local threads = create(...)
-    return run(threads, true)
+    local barrier_ctx = { co = coroutine.running() }
+
+    local can_spawn, threads, count = false, {}, 0
+
+    local function spawn(fn, ...)
+        expect(1, fn, "function")
+
+        threads[count + 1] = {
+            co = coroutine.create(function(...) return exception.try_barrier(barrier_ctx, fn, ...) end),
+            filter = nil,
+            resume_with = table.pack(...),
+        }
+        count = count + 1
+    end
+
+    local function safe_spawn(fn, ...)
+        if not can_spawn then error("Cannot spawn new coroutines outside of waitForAll", 2) end
+        return spawn(fn, ...)
+    end
+
+    local functions = table.pack(...)
+    for i = 1, functions.n, 1 do
+        local fn = functions[i]
+        expect(i, fn, "function")
+        spawn(fn, safe_spawn)
+    end
+
+    local event = { n = 0 }
+    while true do
+        local i = 1
+        while i <= count do
+            local thread = threads[i]
+
+            -- If this is a new coroutine, start it with the "resume_with" data,
+            -- otherwise resume it with the event (if it matches).
+            local resume_with
+            if thread.resume_with then
+                resume_with = thread.resume_with
+                thread.resume_with = nil
+            elseif thread.filter == nil or thread.filter == event[1] or event[1] == "terminate" then
+                resume_with = event
+            end
+
+            if resume_with then
+                can_spawn = true
+                local ok, param = coroutine.resume(thread.co, table.unpack(resume_with, 1, resume_with.n))
+                can_spawn = false
+
+                if not ok then
+                    error(exception.wrap_error(param, thread.co))
+                end
+
+                if coroutine.status(thread.co) == "dead" then
+                    -- If this thread has died, remove it and repeat this
+                    -- iteration.
+                    table.remove(threads, i)
+                    i, count = i - 1, count - 1
+                end
+
+                thread.filter = param
+            end
+
+            i = i + 1
+        end
+
+        if count == 0 then return end
+
+        event = table.pack(os.pullEventRaw())
+    end
 end
--- projects/core/src/test/resources/test-rom/spec/apis/parallel_spec.lua
+++ projects/core/src/test/resources/test-rom/spec/apis/parallel_spec.lua
@@ -30,26 +30,6 @@ describe("The parallel library", function()
             expect(entries):same({ "first", "second", "yield" })
         end)
 
-        it("can summon new functions", function()
-            local entries = {}
-            local function a()
-                entries[#entries + 1] = "a"
-                local s = coroutine.yield()
-                entries[#entries + 1] = s
-            end
-            local function b(summon)
-                entries[#entries + 1] = "b"
-                summon(a)
-                local s = coroutine.yield()
-                entries[#entries + 1] = s
-                summon(a)
-            end
-            os.queueEvent("yield")
-            os.queueEvent("yield")
-            parallel.waitForAny(b)
-            expect(entries):same({ "b", "a", "yield" })
-        end)
-
         it("accepts an arbitrary number of functions", function()
             local count = 0
             local fns = {}
@@ -101,36 +81,52 @@ describe("The parallel library", function()
             local function a()
                 entries[#entries + 1] = "first"
                 local s = coroutine.yield()
-                entries[#entries + 1] = s
+                entries[#entries + 1] = "a: " .. s
             end
             local function b()
                 entries[#entries + 1] = "second"
                 local s = coroutine.yield()
-                entries[#entries + 1] = s
+                entries[#entries + 1] = "b: " .. s
             end
             os.queueEvent("yield")
             parallel.waitForAll(a, b)
-            expect(entries):same({ "first", "second", "yield", "yield" })
+            expect(entries):same({ "first", "second", "a: yield", "b: yield" })
         end)
 
-        it("can summon new functions", function()
+        it("can spawn new functions", function()
             local entries = {}
-            local function a()
-                entries[#entries + 1] = "a"
+            local function a(name)
+                entries[#entries + 1] = name
                 local s = coroutine.yield()
-                entries[#entries + 1] = s
+                entries[#entries + 1] = name .. ": " .. s
             end
-            local function b(summon)
+            local function b(spawn)
                 entries[#entries + 1] = "b"
-                summon(a)
+                spawn(a, "a1")
+                spawn(a, "a2")
                 local s = coroutine.yield()
-                entries[#entries + 1] = s
-                summon(a)
+                entries[#entries + 1] = "b: " .. s
+                spawn(a, "a3")
             end
             os.queueEvent("yield")
             os.queueEvent("yield")
             parallel.waitForAll(b)
-            expect(entries):same({ "b", "a", "yield", "yield", "a", "yield" })
+            expect(entries):same({
+                "b",
+                "a1",
+                "a2",
+                "b: yield",
+                "a1: yield",
+                "a2: yield",
+                "a3",
+                "a3: yield",
+            })
+        end)
+
+        it("cannot spawn outside of parallel", function()
+            local spawn
+            parallel.waitForAll(function(s) spawn = s end)
+            expect.error(spawn, function() end):eq("Cannot spawn new tasks outside of waitForAll")
         end)
 
         it("accepts an arbitrary number of functions", function()
