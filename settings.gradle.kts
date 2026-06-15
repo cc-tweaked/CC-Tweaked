a83385450fa68cb872594033498eea790344237e
Jonathan Coates <git@squiddev.cc>
2026-06-15 18:43:40 +0100

    Add/bump foojay resolver plugin
    We only have this on the 1.21 branch for some reason. Fixes #2457 once
    this is merged across.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,6 +26,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("1.0.0")
+}
+
 val mcVersion: String by settings
 rootProject.name = "cc-tweaked-$mcVersion"
 
