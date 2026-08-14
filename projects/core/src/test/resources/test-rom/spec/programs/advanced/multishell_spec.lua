8bdea64474143a081ac8689c225bcc17bd8be213
Jonathan Coates <git@squiddev.cc>
2026-08-14 16:41:38 +0100

    Fix multishell not culling after changing focus
    When we resume a process, the code looks like:
    
      resumeProcess(nCurrentProcess, ...)
      if cullProcess(nCurrentProcess, ...) then ... end
    
    However, if the focused tab changes while resuming (via
    multishell.setFocus), then we don't cull the correct process!
    
    To fix this, we just add a helper function which resumes+culls, meaning
    we only read the mutable global once.

--- projects/core/src/test/resources/test-rom/spec/programs/advanced/multishell_spec.lua
+++ projects/core/src/test/resources/test-rom/spec/programs/advanced/multishell_spec.lua
@@ -31,4 +31,48 @@ describe("The multishell program", function()
             expect.error(multishell.launch, _ENV, nil):eq("bad argument #2 (string expected, got nil)")
         end)
     end)
+
+    describe("the main loop", function()
+        it("culls a process after exiting", function()
+            local function find_local(co, find)
+                for level = 1, 10 do
+                    for var = 1, 255 do
+                        local name, value = debug.getlocal(co, level, var)
+                        if name == nil then break end
+                        if find == name then return value end
+                    end
+                end
+
+                fail("Cannot find local " .. find)
+            end
+
+            -- We create a very awkward program here which pulls an event (so we can inspect the intermediate state),
+            -- then switches to a different tab before exiting, causing nCurrentProcess to change in the multishell
+            -- loop.
+            io.open("/test-files/a.lua", "w"):write([[os.pullEvent("mouse_click"); multishell.setFocus(1)]]):close()
+
+            local window = window.create(term.current(), 1, 1, 51, 19, false)
+            local co = coroutine.create(function() os.run({ shell = shell }, "rom/programs/advanced/multishell.lua") end)
+            local function resume(...)
+                local old = term.redirect(window)
+                local ok, err = coroutine.resume(co, ...)
+                term.redirect(old)
+                if not ok then fail("Coroutine failed: " .. err) end
+            end
+
+            resume()
+            local multishell = find_local(co, "multishell")
+            expect(multishell.getFocus()):eq(1)
+            expect(multishell.getCount()):eq(1)
+
+            resume("paste", "fg /test-files/a.lua")
+            resume("key", keys.enter, false)
+            expect(multishell.getFocus()):eq(2)
+            expect(multishell.getCount()):eq(2)
+
+            resume("mouse_click", 1, 1, 2)
+            expect(multishell.getFocus()):eq(1)
+            expect(multishell.getCount()):eq(1)
+        end)
+    end)
 end)
